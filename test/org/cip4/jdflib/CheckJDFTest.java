dbcd7bbf722b272bc59966f2762c72e83e273344
mucha <mucha@cip4.org>
2007-02-20 10:19:02 +0000

    git-svn-id: http://svn.cip4.org/svnroot/trunk@1766 25ee8a6d-831b-0410-907a-ff8528d65d49

--- test/org/cip4/jdflib/CheckJDFTest.java
+++ test/org/cip4/jdflib/CheckJDFTest.java
@@ -78,7 +78,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
