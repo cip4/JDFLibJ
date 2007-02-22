6b78c306c2af624ebf2613b9d80dd838ef98ceba
Rainer Prosi <Rainer.Prosi@heidelberg.com>
2007-02-22 09:40:39 +0000

    git-svn-id: http://svn.cip4.org/svnroot/trunk@1780 25ee8a6d-831b-0410-907a-ff8528d65d49

---
+++ test/org/cip4/jdflib/jmf/JMFKnownMessagesTest.java
@@ -0,0 +1,98 @@
+package org.cip4.jdflib.jmf;
+
+import org.cip4.jdflib.JDFTestCaseBase;
+import org.cip4.jdflib.auto.JDFAutoDeviceInfo.EnumDeviceStatus;
+import org.cip4.jdflib.auto.JDFAutoMessageService.EnumJMFRole;
+import org.cip4.jdflib.core.AttributeName;
+import org.cip4.jdflib.core.ElementName;
+import org.cip4.jdflib.core.JDFDoc;
+import org.cip4.jdflib.core.XMLDoc;
+import org.cip4.jdflib.core.KElement.EnumValidationLevel;
+import org.cip4.jdflib.datatypes.JDFBaseDataTypes.EnumFitsValue;
+import org.cip4.jdflib.jmf.JDFMessage.EnumType;
+import org.cip4.jdflib.resource.JDFDeviceList;
+import org.cip4.jdflib.resource.devicecapability.JDFDevCap;
+import org.cip4.jdflib.resource.devicecapability.JDFDevCaps;
+import org.cip4.jdflib.resource.devicecapability.JDFDeviceCap;
+import org.cip4.jdflib.resource.devicecapability.JDFStringState;
+
+/**
+ * @author Rainer Prosi
+ *
+ * Test of the Resource JMF
+ */
+public class JMFKnownMessagesTest extends JDFTestCaseBase
+{
+    public void testJMFDevCaps()
+    {
+        JDFDoc doc = new JDFDoc(ElementName.JMF);
+        JDFJMF jmfDC=doc.getJMFRoot();
+       
+        JDFResponse r=jmfDC.appendResponse(EnumType.KnownMessages);
+        jmfDC.setSenderID("DeviceSenderID");
+        
+        JDFMessageService ms=r.appendMessageService();
+        ms.setJMFRole(EnumJMFRole.Sender);
+        ms.setQuery(true);
+        ms.setType("KnownDevices");
+        JDFDevCaps dcs=ms.appendDevCaps();
+        dcs.setName("DeviceList");
+        JDFDevCap dc=dcs.appendDevCapInPool();
+        JDFDevCap dcDI=dc.appendDevCap();
+        dcDI.setName(ElementName.DEVICEINFO);
+        dcDI.setMinOccurs(1);
+        dcDI.setMaxOccurs(1);
+        JDFStringState state=dcDI.appendStringState(AttributeName.DEVICEID);
+        state.setRequired(true);
+        
+        state=dcDI.appendStringState(AttributeName.DEVICESTATUS);
+        state.setRequired(true);
+        state.appendValueAllowedValue("Running");
+        
+        ms=r.appendMessageService();
+        ms.setJMFRole(EnumJMFRole.Sender);
+        ms.setQuery(true);
+        ms.setType("KnownMessages");
+        dcs=ms.appendDevCaps();
+        dcs.setName("MessageService");
+        dc=dcs.appendDevCapInPool();
+        state=dc.appendStringState(AttributeName.TYPE);
+        state.setRequired(true);
+        
+        
+        
+        
+        JDFDoc docJMF=new JDFDoc("JMF");
+        JDFJMF jmf=docJMF.getJMFRoot();
+        for(int i=0;i<3;i++)
+        {
+            JDFResponse resp=jmf.appendResponse(EnumType.KnownDevices);
+            JDFDeviceList dl=resp.appendDeviceList();
+            JDFDeviceInfo di=dl.appendDeviceInfo();
+            di.setDeviceID("d123");
+            di.setDeviceStatus(EnumDeviceStatus.Running);
+            XMLDoc report=JDFDeviceCap.getJMFInfo(jmf, r, EnumFitsValue.Allowed, EnumValidationLevel.Complete, true);
+            assertEquals(report.getRoot().getAttribute("IsValid"),"true");
+        }
+        {
+            JDFResponse resp=jmf.appendResponse(EnumType.KnownMessages);
+            JDFMessageService mi=resp.appendMessageService();
+            mi.setType("FooBar");
+            doc.write2File(sm_dirTestDataTemp+"JMFDevCap.xml", 2, false);
+            docJMF.write2File(sm_dirTestDataTemp+"JMFDevCapTest.jmf", 2, false);
+            
+        }
+        XMLDoc report=JDFDeviceCap.getJMFInfo(jmf, r, EnumFitsValue.Allowed, EnumValidationLevel.Complete, true);
+        assertEquals(report.getRoot().getAttribute("IsValid"),"true");
+        
+        doc.write2File(sm_dirTestDataTemp+"JMFDevCap.xml", 2, false);
+        docJMF.write2File(sm_dirTestDataTemp+"JMFDevCapTest.jmf", 2, false);
+        {
+            JDFResponse resp=jmf.appendResponse(EnumType.AbortQueueEntry);             
+        }
+        report=JDFDeviceCap.getJMFInfo(jmf, r, EnumFitsValue.Allowed, EnumValidationLevel.Complete, true);
+        assertEquals(report.getRoot().getAttribute("IsValid"),"false");
+ 
+    }       
+        /////////////////////////////////////////////////////////////////////
+}
