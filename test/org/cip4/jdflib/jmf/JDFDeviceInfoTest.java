fc7ca29d928d92708a03ecdf08750bd7d4d2dfdd
Rainer Prosi <Rainer.Prosi@heidelberg.com>
2007-09-20 07:10:05 +0000

    git-svn-id: http://svn.cip4.org/svnroot/trunk@2607 25ee8a6d-831b-0410-907a-ff8528d65d49

---
+++ test/org/cip4/jdflib/jmf/JDFDeviceInfoTest.java
@@ -0,0 +1,122 @@
+/*
+ * The CIP4 Software License, Version 1.0
+ *
+ *
+ * Copyright (c) 2001-2007 The International Cooperation for the Integration of
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        The International Cooperation for the Integration of
+ *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of
+ *    Processes in  Prepress, Press and Postpress" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact info@cip4.org.
+ *
+ * 5. Products derived from this software may not be called "CIP4",
+ *    nor may "CIP4" appear in their name, without prior written
+ *    permission of the CIP4 organization
+ *
+ * Usage of this software in commercial products is subject to restrictions. For
+ * details please consult info@cip4.org.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR
+ * THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the The International Cooperation for the Integration
+ * of Processes in Prepress, Press and Postpress and was
+ * originally based on software
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the
+ * Integration of Processes in  Prepress, Press and Postpress , please see
+ * <http://www.cip4.org/>.
+ *
+ *
+ */
+
+package org.cip4.jdflib.jmf;
+
+import java.io.File;
+
+import org.cip4.jdflib.JDFTestCaseBase;
+import org.cip4.jdflib.auto.JDFAutoDeviceInfo.EnumDeviceCondition;
+import org.cip4.jdflib.auto.JDFAutoDeviceInfo.EnumDeviceStatus;
+import org.cip4.jdflib.auto.JDFAutoMISDetails.EnumWorkType;
+import org.cip4.jdflib.core.ElementName;
+import org.cip4.jdflib.core.JDFDoc;
+import org.cip4.jdflib.core.JDFElement;
+import org.cip4.jdflib.core.JDFElement.EnumNodeStatus;
+import org.cip4.jdflib.core.KElement.EnumValidationLevel;
+import org.cip4.jdflib.jmf.JDFMessage.EnumType;
+import org.cip4.jdflib.pool.JDFAuditPool;
+import org.cip4.jdflib.resource.JDFPhaseTime;
+import org.cip4.jdflib.resource.process.JDFMISDetails;
+
+/**
+ * @author Rainer Prosi
+ *
+ * Test of the Status JMF
+ */
+public class JDFDeviceInfoTest extends JDFTestCaseBase
+{
+    private  JDFDeviceInfo di;
+
+    public void setUp()
+    {
+        JDFElement.setLongID(false);
+        JDFDoc doc = new JDFDoc(ElementName.DEVICEINFO);
+        di=(JDFDeviceInfo) doc.getRoot();
+        
+    }
+    /////////////////////////////////////////////////////////////////////
+
+    public void testJobPhaseFromPhaseTime()
+    {
+        JDFDoc d=new JDFDoc("JDF");
+        JDFAuditPool ap=d.getJDFRoot().getCreateAuditPool();
+        JDFPhaseTime pt=ap.setPhase(EnumNodeStatus.InProgress,"dummy",null);
+        JDFJobPhase jp=di.createJobPhaseFromPhaseTime(pt);
+        assertFalse(pt.hasChildElement(ElementName.MISDETAILS, null));
+        final JDFMISDetails misDetails = pt.appendMISDetails();
+        misDetails.setWorkTypeDetails("FooBar");
+        misDetails.setWorkType(EnumWorkType.Alteration);
+        jp=di.createJobPhaseFromPhaseTime(pt);
+        assertEquals(pt.getMISDetails().getWorkType(),jp.getMISDetails().getWorkType());
+            
+    }
+
+}
