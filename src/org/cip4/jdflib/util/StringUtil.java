9554836fc49f5e7ca2aef6c588dc5cf401b7b355
mucha <mucha@cip4.org>
2007-10-05 12:31:54 +0000

    Back to Java 1.4 Compatibility
    git-svn-id: http://svn.cip4.org/svnroot/trunk@2673 25ee8a6d-831b-0410-907a-ff8528d65d49

--- src/org/cip4/jdflib/util/StringUtil.java
+++ src/org/cip4/jdflib/util/StringUtil.java
@@ -91,6 +91,8 @@ import java.util.zip.DataFormatException;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.enums.EnumUtils;
 import org.apache.commons.lang.enums.ValuedEnum;
+import org.apache.crimson.util.XmlNames;		// for Java up to 1.4
+// import com.sun.imageio.metadata.XmlNames;	// for Java from 5.0 onward
 import org.cip4.jdflib.cformat.PrintfFormat;
 import org.cip4.jdflib.core.AttributeInfo;
 import org.cip4.jdflib.core.JDFConstants;
@@ -106,8 +108,6 @@ import org.cip4.jdflib.datatypes.JDFNumberRangeList;
 import org.cip4.jdflib.datatypes.JDFXYPair;
 import org.cip4.jdflib.datatypes.JDFXYPairRange;
 import org.cip4.jdflib.datatypes.JDFXYPairRangeList;
-
-import com.sun.imageio.metadata.XmlNames;
 
 
 
