eba48dfb38f4b73b79da7eba9ae0995125e8c61a
mucha <mucha@cip4.org>
2007-10-23 08:39:47 +0000

    JDFException : removed deprecated methods     public JDFException (String message, boolean bPrintStack)      * @deprecated print the stack trace in the application
    git-svn-id: http://svn.cip4.org/svnroot/trunk@2738 25ee8a6d-831b-0410-907a-ff8528d65d49

--- src/org/cip4/jdflib/core/JDFException.java
+++ src/org/cip4/jdflib/core/JDFException.java
@@ -110,30 +110,14 @@ public class JDFException extends RuntimeException
         id=_id;
     }
     
-    /**
-     * constructor
-     *
-     * @param String message
-     * @param boolean bPrintStack print Stacktrace if true
-     * @deprecated print the stack trace in the application
-     */
-    public JDFException (String message, boolean bPrintStack)
-    {
-        super(message);
-        id=hashCode();
-        if (bPrintStack == true)
-        {
-            printStackTrace();
-        }
-    }
-
     //**************************************** Methods *********************************************
     /**
      * toString - StringRepresentation of JDFNode
      *
      * @return String
      */
-    public String toString()
+    @Override
+	public String toString()
     {
         return "JDFException[  --> " + super.toString() + " ]";
     }
