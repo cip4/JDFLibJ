206dbd14d9f62f885b0a6c7feca7ac10018b159f
Rainer Prosi <Rainer.Prosi@heidelberg.com>
2008-12-12 14:29:46 +0000

    git-svn-id: http://svn.cip4.org/svnroot/trunk@3725 25ee8a6d-831b-0410-907a-ff8528d65d49

---
+++ test/org/cip4/jdflib/extensions/XJDFTest.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package org.cip4.jdflib.extensions;
+
+import org.cip4.jdflib.JDFTestCaseBase;
+import org.cip4.jdflib.auto.JDFAutoExposedMedia.EnumPlateType;
+import org.cip4.jdflib.core.JDFDoc;
+import org.cip4.jdflib.core.JDFResourceLink;
+import org.cip4.jdflib.core.KElement;
+import org.cip4.jdflib.core.JDFResourceLink.EnumUsage;
+import org.cip4.jdflib.extensions.xjdfwalker.XJDFToJDFConverter;
+import org.cip4.jdflib.node.JDFNode;
+import org.cip4.jdflib.node.JDFNode.EnumType;
+import org.cip4.jdflib.resource.JDFResource;
+import org.cip4.jdflib.resource.JDFResource.EnumPartIDKey;
+import org.cip4.jdflib.resource.process.JDFExposedMedia;
+
+/**
+ * @author Rainer Prosi, Heidelberger Druckmaschinen
+ * 
+ */
+public class XJDFTest extends JDFTestCaseBase
+{
+
+	/**
+	 * @throws Exception
+	 */
+	public void testToXJDF() throws Exception
+	{
+		final JDFNode n = new JDFDoc("JDF").getJDFRoot();
+		n.setType(EnumType.ConventionalPrinting);
+
+		final JDFResource r = n.addResource("ExposedMedia", EnumUsage.Input);
+		final JDFResource r2 = r.addPartition(EnumPartIDKey.SignatureName, "sig1");
+		final JDFResource r3 = r2.addPartition(EnumPartIDKey.SheetName, "s1");
+		r3.setProductID("P1");
+		final JDFExposedMedia xm0 = (JDFExposedMedia) r3;
+		xm0.setPlateType(EnumPlateType.Dummy);
+		final KElement e = XJDF20.makeNewJDF(n, n);
+
+		final JDFNode n2 = new JDFDoc("JDF").getJDFRoot();
+		n2.setType(EnumType.ConventionalPrinting);
+		final XJDFToJDFConverter xCon = new XJDFToJDFConverter(n2.getOwnerDocument_JDFElement());
+		final JDFDoc d2 = xCon.convert(e);
+		assertNotNull(d2);
+		final JDFNode nConv = d2.getJDFRoot();
+		assertNotNull(nConv);
+		JDFExposedMedia xm = (JDFExposedMedia) nConv.getResource("ExposedMedia", EnumUsage.Input, 0);
+		final JDFResourceLink rl = nConv.getLink(xm, null);
+		assertNotNull(rl);
+		xm = (JDFExposedMedia) rl.getTarget();
+		assertEquals(xm.getProductID(), "P1");
+		assertEquals(xm.getPlateType(), EnumPlateType.Dummy);
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	public void testFromXJDF() throws Exception
+	{
+		final JDFDoc d = new JDFDoc("JDF");
+		final XJDFToJDFConverter xc = new XJDFToJDFConverter(d);
+	}
+
+}
