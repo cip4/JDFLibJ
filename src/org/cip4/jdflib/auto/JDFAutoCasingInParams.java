9e64daab92dff25e8ae935e2e677784c38e7f8d4
mucha <mucha@cip4.org>
2009-09-04 11:32:02 +0000

    clean up of auto files
    git-svn-id: http://svn.cip4.org/svnroot/trunk@4066 25ee8a6d-831b-0410-907a-ff8528d65d49

--- src/org/cip4/jdflib/auto/JDFAutoCasingInParams.java
+++ src/org/cip4/jdflib/auto/JDFAutoCasingInParams.java
@@ -70,12 +70,21 @@
 
 package org.cip4.jdflib.auto;
 
-import java.util.Collection;                        
-import java.util.Vector;                            
-import org.apache.xerces.dom.CoreDocumentImpl;      
-import org.cip4.jdflib.core.*;                      
-import org.cip4.jdflib.resource.*;                  
-import org.cip4.jdflib.resource.process.postpress.*;
+import java.util.Collection;
+import java.util.Vector;
+
+import org.apache.xerces.dom.CoreDocumentImpl;
+import org.cip4.jdflib.core.AtrInfoTable;
+import org.cip4.jdflib.core.AttributeInfo;
+import org.cip4.jdflib.core.AttributeName;
+import org.cip4.jdflib.core.ElemInfoTable;
+import org.cip4.jdflib.core.ElementInfo;
+import org.cip4.jdflib.core.ElementName;
+import org.cip4.jdflib.core.JDFException;
+import org.cip4.jdflib.core.VElement;
+import org.cip4.jdflib.resource.JDFResource;
+import org.cip4.jdflib.resource.process.postpress.JDFGlueApplication;
+import org.cip4.jdflib.resource.process.postpress.JDFGlueLine;
 
 public abstract class JDFAutoCasingInParams extends JDFResource
 {
