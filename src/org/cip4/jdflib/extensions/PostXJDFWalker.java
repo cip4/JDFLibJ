974e7c5d8a78c1d57d98144c64c062f7f1f0c0b9
Rainer Prosi <Rainer.Prosi@heidelberg.com>
2009-11-05 20:25:07 +0000

    added some helper classes
    git-svn-id: http://svn.cip4.org/svnroot/trunk@4257 25ee8a6d-831b-0410-907a-ff8528d65d49

---
+++ src/org/cip4/jdflib/extensions/PostXJDFWalker.java
@@ -0,0 +1,162 @@
+/**
+ * 
+ */
+package org.cip4.jdflib.extensions;
+
+import org.cip4.jdflib.core.AttributeName;
+import org.cip4.jdflib.core.ElementName;
+import org.cip4.jdflib.core.KElement;
+import org.cip4.jdflib.datatypes.VJDFAttributeMap;
+import org.cip4.jdflib.elementwalker.BaseElementWalker;
+import org.cip4.jdflib.elementwalker.BaseWalker;
+import org.cip4.jdflib.elementwalker.BaseWalkerFactory;
+
+/**
+  * @author Rainer Prosi, Heidelberger Druckmaschinen *
+ */
+class PostXJDFWalker extends BaseElementWalker
+{
+	/**
+	 * if true merge stripping and layout
+	 */
+	boolean mergeLayout = true;
+	KElement newRoot;
+
+	/**
+	 * 
+	 * @author Rainer Prosi, Heidelberger Druckmaschinen
+	 * 
+	 */
+	protected class WalkElement extends BaseWalker
+	{
+		@SuppressWarnings("synthetic-access")
+		public WalkElement()
+		{
+			super(getFactory());
+		}
+
+		/**
+		 * @param xjdf
+		 * @return true if must continue
+		 */
+		@Override
+		public KElement walk(final KElement xjdf, final KElement dummy)
+		{
+			return xjdf;
+		}
+	}
+
+	/**
+	 * 
+	 * @author Rainer Prosi, Heidelberger Druckmaschinen
+	 * 
+	 */
+	protected class WalkStrippingParams extends WalkElement
+	{
+		/**
+		 * 
+		 */
+		public WalkStrippingParams()
+		{
+			super();
+		}
+
+		/**
+		 * @see org.cip4.jdflib.elementwalker.BaseWalker#matches(org.cip4.jdflib.core.KElement)
+		 * @param toCheck
+		 * @return true if it matches
+		 */
+		@Override
+		public boolean matches(final KElement toCheck)
+		{
+			return toCheck.getLocalName().equals(ElementName.STRIPPINGPARAMS);
+		}
+
+		/**
+		 * @see org.cip4.jdflib.extensions.XJDF20.WalkResource#walk(org.cip4.jdflib.core.KElement, org.cip4.jdflib.core.KElement)
+		 * @param xjdf
+		 * @param dummy
+		 * @return
+		*/
+		@Override
+		public KElement walk(KElement xjdf, KElement dummy)
+		{
+			if (!mergeLayout)
+			{
+				return xjdf; // nuff done
+			}
+			KElement newLayout = newRoot.getXPathElement("ParameterSet[@Name=\"Layout\"]");
+			if (newLayout != null)
+			{
+				VJDFAttributeMap vmap = new PartitionHelper(xjdf.getParentNode_KElement()).getPartMapVector();
+				KElement layoutPartition = new SetHelper(newLayout).getPartition(vmap);
+				if (layoutPartition != null)
+				{
+					layoutPartition = layoutPartition.getCreateElement("Layout");
+					layoutPartition.mergeElement(xjdf, false); // ret is the actual strippingparams
+				}
+			}
+			return null; // stop after merging
+
+		}
+
+	}
+
+	/**
+	 * 
+	 * @author Rainer Prosi, Heidelberger Druckmaschinen
+	 * 
+	 */
+	protected class WalkStrippingSet extends WalkElement
+	{
+		/**
+		 * 
+		 */
+		public WalkStrippingSet()
+		{
+			super();
+		}
+
+		/**
+		 * @see org.cip4.jdflib.elementwalker.BaseWalker#matches(org.cip4.jdflib.core.KElement)
+		 * @param toCheck
+		 * @return true if it matches
+		 */
+		@Override
+		public boolean matches(final KElement toCheck)
+		{
+			return toCheck.getLocalName().equals("ParameterSet")
+					&& ElementName.STRIPPINGPARAMS.equals(toCheck.getAttribute(AttributeName.NAME));
+		}
+
+		/**
+		 * @see org.cip4.jdflib.extensions.XJDF20.WalkResource#walk(org.cip4.jdflib.core.KElement, org.cip4.jdflib.core.KElement)
+		 * @param xjdf
+		 * @param dummy
+		 * @return
+		*/
+		@Override
+		public KElement walk(KElement xjdf, KElement dummy)
+		{
+			if (mergeLayout)
+			{
+				xjdf.deleteNode();
+			}
+
+			return mergeLayout ? null : xjdf; // stop after merging
+
+		}
+
+	}
+
+	/**
+	 * @param newRoot 
+	 *  
+	 */
+	public PostXJDFWalker(KElement newRoot)
+	{
+		super(new BaseWalkerFactory());
+		this.newRoot = newRoot;
+	}
+
+}
