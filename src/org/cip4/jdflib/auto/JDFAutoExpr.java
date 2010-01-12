24c122ac8455139072af4ae16064679c162d34fe
mucha <mucha@cip4.org>
2010-01-12 10:46:08 +0000

    added JDFExpr and JDFBindingQualityMeasurement
    git-svn-id: http://svn.cip4.org/svnroot/trunk@4405 25ee8a6d-831b-0410-907a-ff8528d65d49

---
+++ src/org/cip4/jdflib/auto/JDFAutoExpr.java
@@ -0,0 +1,298 @@
+/*
+ * The CIP4 Software License, Version 1.0
+ *
+ *
+ * Copyright (c) 2001-2005 The International Cooperation for the Integration of
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        The International Cooperation for the Integration of
+ *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of
+ *    Processes in  Prepress, Press and Postpress" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact info@cip4.org.
+ *
+ * 5. Products derived from this software may not be called "CIP4",
+ *    nor may "CIP4" appear in their name, without prior written
+ *    permission of the CIP4 organization
+ *
+ * Usage of this software in commercial products is subject to restrictions. For
+ * details please consult info@cip4.org.
+  *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR
+ * THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the The International Cooperation for the Integration
+ * of Processes in Prepress, Press and Postpress and was
+ * originally based on software
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the
+ * Integration of Processes in  Prepress, Press and Postpress , please see
+ * <http://www.cip4.org/>.
+ *
+ *
+ */
+
+package org.cip4.jdflib.auto;
+
+import java.util.Collection;
+import java.util.Vector;
+
+import org.apache.xerces.dom.CoreDocumentImpl;
+import org.cip4.jdflib.core.AtrInfoTable;
+import org.cip4.jdflib.core.AttributeInfo;
+import org.cip4.jdflib.core.AttributeName;
+import org.cip4.jdflib.core.ElemInfoTable;
+import org.cip4.jdflib.core.ElementInfo;
+import org.cip4.jdflib.core.ElementName;
+import org.cip4.jdflib.core.JDFConstants;
+import org.cip4.jdflib.core.JDFElement;
+import org.cip4.jdflib.core.JDFException;
+import org.cip4.jdflib.core.VElement;
+import org.cip4.jdflib.resource.devicecapability.JDFTerm;
+
+public abstract class JDFAutoExpr extends JDFElement
+{
+
+    private static final long serialVersionUID = 1L;
+
+    private static AtrInfoTable[] atrInfoTable = new AtrInfoTable[3];
+    static
+    {
+        atrInfoTable[0] = new AtrInfoTable(AttributeName.NAME, 0x33333333, AttributeInfo.EnumAttributeType.NMTOKEN, null, null);
+        atrInfoTable[1] = new AtrInfoTable(AttributeName.PATH, 0x33333333, AttributeInfo.EnumAttributeType.XPath, null, null);
+        atrInfoTable[2] = new AtrInfoTable(AttributeName.VALUE, 0x33333333, AttributeInfo.EnumAttributeType.string, null, null);
+    }
+    
+    @Override
+	protected AttributeInfo getTheAttributeInfo()
+    {
+        return super.getTheAttributeInfo().updateReplace(atrInfoTable);
+    }
+
+
+    private static ElemInfoTable[] elemInfoTable = new ElemInfoTable[1];
+    static
+    {
+        elemInfoTable[0] = new ElemInfoTable(ElementName.TERM, 0x33333333);
+    }
+    
+    @Override
+	protected ElementInfo getTheElementInfo()
+    {
+        return super.getTheElementInfo().updateReplace(elemInfoTable);
+    }
+
+
+
+    /**
+     * Constructor for JDFAutoExpr
+     * @param myOwnerDocument
+     * @param qualifiedName
+     */
+    protected JDFAutoExpr(
+        CoreDocumentImpl myOwnerDocument,
+        String qualifiedName)
+    {
+        super(myOwnerDocument, qualifiedName);
+    }
+
+    /**
+     * Constructor for JDFAutoExpr
+     * @param myOwnerDocument
+     * @param myNamespaceURI
+     * @param qualifiedName
+     */
+    protected JDFAutoExpr(
+        CoreDocumentImpl myOwnerDocument,
+        String myNamespaceURI,
+        String qualifiedName)
+    {
+        super(myOwnerDocument, myNamespaceURI, qualifiedName);
+    }
+
+    /**
+     * Constructor for JDFAutoExpr
+     * @param myOwnerDocument
+     * @param myNamespaceURI
+     * @param qualifiedName
+     * @param myLocalName
+     */
+    protected JDFAutoExpr(
+        CoreDocumentImpl myOwnerDocument,
+        String myNamespaceURI,
+        String qualifiedName,
+        String myLocalName)
+    {
+        super(myOwnerDocument, myNamespaceURI, qualifiedName, myLocalName);
+    }
+
+
+    @Override
+	public String toString()
+    {
+        return " JDFAutoExpr[  --> " + super.toString() + " ]";
+    }
+
+
+/* ************************************************************************
+ * Attribute getter / setter
+ * ************************************************************************
+ */
+        
+        /* ---------------------------------------------------------------------
+        Methods for Attribute Name
+        --------------------------------------------------------------------- */
+        /**
+          * (36) set attribute Name
+          * @param value: the value to set the attribute to
+          */
+        public void setName(String value)
+        {
+            setAttribute(AttributeName.NAME, value, null);
+        }
+
+        /**
+          * (23) get String attribute Name
+          * @return the value of the attribute
+          */
+        public String getName()
+        {
+            return getAttribute(AttributeName.NAME, null, JDFConstants.EMPTYSTRING);
+        }
+
+        
+        /* ---------------------------------------------------------------------
+        Methods for Attribute Path
+        --------------------------------------------------------------------- */
+        /**
+          * (36) set attribute Path
+          * @param value: the value to set the attribute to
+          */
+        public void setPath(String value)
+        {
+            setAttribute(AttributeName.PATH, value, null);
+        }
+
+        /**
+          * (23) get String attribute Path
+          * @return the value of the attribute
+          */
+        public String getPath()
+        {
+            return getAttribute(AttributeName.PATH, null, JDFConstants.EMPTYSTRING);
+        }
+
+        
+        /* ---------------------------------------------------------------------
+        Methods for Attribute Value
+        --------------------------------------------------------------------- */
+        /**
+          * (36) set attribute Value
+          * @param value: the value to set the attribute to
+          */
+        public void setValue(String value)
+        {
+            setAttribute(AttributeName.VALUE, value, null);
+        }
+
+        /**
+          * (23) get String attribute Value
+          * @return the value of the attribute
+          */
+        public String getValue()
+        {
+            return getAttribute(AttributeName.VALUE, null, JDFConstants.EMPTYSTRING);
+        }
+
+/* ***********************************************************************
+ * Element getter / setter
+ * ***********************************************************************
+ */
+
+    /** (26) getCreateTerm
+     * 
+     * @param iSkip number of elements to skip
+     * @return JDFTerm the element
+     */
+    public JDFTerm getCreateTerm(int iSkip)
+    {
+        return (JDFTerm)getCreateElement_KElement(ElementName.TERM, null, iSkip);
+    }
+
+    /**
+     * (27) const get element Term
+     * @param iSkip number of elements to skip
+     * @return JDFTerm the element
+     * default is getTerm(0)     */
+    public JDFTerm getTerm(int iSkip)
+    {
+        return (JDFTerm) getElement(ElementName.TERM, null, iSkip);
+    }
+
+    /**
+     * Get all Term from the current element
+     * 
+     * @return Collection<JDFTerm>, null if none are available
+     */
+    public Collection<JDFTerm> getAllTerm()
+    {
+        final VElement vc = getChildElementVector(ElementName.TERM, null);
+        if (vc == null || vc.size() == 0)
+        {
+            return null;
+        }
+
+        final Vector<JDFTerm> v = new Vector<JDFTerm>();
+        for (int i = 0; i < vc.size(); i++)
+        {
+            v.add((JDFTerm) vc.get(i));
+        }
+
+        return v;
+    }
+
+    /**
+     * (30) append element Term
+     */
+    public JDFTerm appendTerm() throws JDFException
+    {
+        return (JDFTerm) appendElement(ElementName.TERM, null);
+    }
+
+}// end namespace JDF
