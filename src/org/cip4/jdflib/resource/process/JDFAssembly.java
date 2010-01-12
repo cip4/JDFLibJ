61dc628a2566dd3a0119a90b8b454316993c59a7
Rainer Prosi <Rainer.Prosi@heidelberg.com>
2010-01-12 19:00:46 +0000

    fixed order deprecation
    git-svn-id: http://svn.cip4.org/svnroot/trunk@4435 25ee8a6d-831b-0410-907a-ff8528d65d49

--- src/org/cip4/jdflib/resource/process/JDFAssembly.java
+++ src/org/cip4/jdflib/resource/process/JDFAssembly.java
@@ -9,10 +9,25 @@ package org.cip4.jdflib.resource.process;
 
 import org.apache.xerces.dom.CoreDocumentImpl;
 import org.cip4.jdflib.auto.JDFAutoAssembly;
+import org.cip4.jdflib.core.AtrInfoTable;
+import org.cip4.jdflib.core.AttributeInfo;
+import org.cip4.jdflib.core.AttributeName;
 
 public class JDFAssembly extends JDFAutoAssembly
 {
 	private static final long serialVersionUID = 1L;
+
+	private static AtrInfoTable[] atrInfoTable = new AtrInfoTable[1];
+	static
+	{
+		atrInfoTable[0] = new AtrInfoTable(AttributeName.ORDER, 0x33333311, AttributeInfo.EnumAttributeType.enumeration, EnumOrder.getEnum(0), "Gathering");
+	}
+
+	@Override
+	protected AttributeInfo getTheAttributeInfo()
+	{
+		return super.getTheAttributeInfo().updateReplace(atrInfoTable);
+	}
 
 	/**
 	 * Constructor for JDFAssembly
@@ -32,8 +47,7 @@ public class JDFAssembly extends JDFAutoAssembly
 	 * @param namespaceURI
 	 * @param qualifiedName
 	 */
-	public JDFAssembly(CoreDocumentImpl myOwnerDocument, String myNamespaceURI,
-			String qualifiedName)
+	public JDFAssembly(CoreDocumentImpl myOwnerDocument, String myNamespaceURI, String qualifiedName)
 	{
 		super(myOwnerDocument, myNamespaceURI, qualifiedName);
 	}
@@ -46,8 +60,7 @@ public class JDFAssembly extends JDFAutoAssembly
 	 * @param qualifiedName
 	 * @param localName
 	 */
-	public JDFAssembly(CoreDocumentImpl myOwnerDocument, String myNamespaceURI,
-			String qualifiedName, String myLocalName)
+	public JDFAssembly(CoreDocumentImpl myOwnerDocument, String myNamespaceURI, String qualifiedName, String myLocalName)
 	{
 		super(myOwnerDocument, myNamespaceURI, qualifiedName, myLocalName);
 	}
