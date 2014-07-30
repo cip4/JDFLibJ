a954b67a1215e26d598dc51dd2142744226b1ee1
Rainer Prosi <rainer.prosi@heidelberg.com>
2014-07-30 12:41:39 +0200

    should be final test fix - capitalisation of file name in test class

--- src/test/java/org/cip4/jdflib/core/XMLDocTest.java
+++ src/test/java/org/cip4/jdflib/core/XMLDocTest.java
@@ -200,9 +200,9 @@ public class XMLDocTest extends JDFTestCaseBase
 		@Override
 		public void runMyThread()
 		{
-			System.out.println("parsing " + sm_dirTestData + "bigwhite.jdf " + iLoop);
-			XMLDoc d = XMLDoc.parseFile(sm_dirTestData + "bigwhite.jdf");
-			assertNotNull("parsed " + sm_dirTestData + "bigwhite.jdf " + iLoop, d);
+			System.out.println("parsing " + sm_dirTestData + "bigWhite.jdf " + iLoop);
+			XMLDoc d = XMLDoc.parseFile(sm_dirTestData + "bigWhite.jdf");
+			assertNotNull("parsed " + sm_dirTestData + "bigWhite.jdf " + iLoop, d);
 		}
 	}
 
