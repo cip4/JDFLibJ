04e8b763009881a14554dcbba6318c36e5a41e1c
Rainer Prosi <rainer.prosi@heidelberg.com>
2017-01-08 21:35:20 +0100

    test fixes

--- src/test/java/org/cip4/jdflib/extensions/examples/XJDFSheetOptimizeTest.java
+++ src/test/java/org/cip4/jdflib/extensions/examples/XJDFSheetOptimizeTest.java
@@ -1,8 +1,8 @@
 /**
  * The CIP4 Software License, Version 1.0
  *
- * Copyright (c) 2001-2016 The International Cooperation for the Integration of 
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
+ * Copyright (c) 2001-2017 The International Cooperation for the Integration of
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -10,7 +10,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -18,17 +18,17 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
- *        The International Cooperation for the Integration of 
+ *        The International Cooperation for the Integration of
  *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of 
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of
  *    Processes in  Prepress, Press and Postpress" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact info@cip4.org.
  *
  * 5. Products derived from this software may not be called "CIP4",
@@ -54,22 +54,21 @@
  * ====================================================================
  *
  * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration 
+ * individuals on behalf of the The International Cooperation for the Integration
  * of Processes in Prepress, Press and Postpress and was
- * originally based on software 
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG 
- * copyright (c) 1999-2001, Agfa-Gevaert N.V. 
- *  
- * For more information on The International Cooperation for the 
+ * originally based on software
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the
  * Integration of Processes in  Prepress, Press and Postpress , please see
  * <http://www.cip4.org/>.
- *  
- * 
+ *
+ *
  */
 package org.cip4.jdflib.extensions.examples;
 
 import org.cip4.jdflib.JDFTestCaseBase;
-import org.cip4.jdflib.auto.JDFAutoStrippingParams.EnumWorkStyle;
 import org.cip4.jdflib.core.AttributeName;
 import org.cip4.jdflib.core.ElementName;
 import org.cip4.jdflib.core.JDFResourceLink.EnumUsage;
@@ -77,16 +76,15 @@ import org.cip4.jdflib.core.KElement;
 import org.cip4.jdflib.datatypes.JDFAttributeMap;
 import org.cip4.jdflib.extensions.ResourceHelper;
 import org.cip4.jdflib.extensions.SetHelper;
+import org.cip4.jdflib.extensions.XJDFConstants;
 import org.cip4.jdflib.extensions.XJDFHelper;
-import org.cip4.jdflib.resource.JDFResource.EnumPartIDKey;
-import org.cip4.jdflib.resource.process.JDFBinderySignature;
 import org.cip4.jdflib.resource.process.JDFConvertingConfig;
 import org.cip4.jdflib.resource.process.JDFLayout;
 import org.cip4.jdflib.resource.process.JDFPosition;
 import org.junit.Test;
 
 /**
- * 
+ *
  * @author rainerprosi
  * @date Dec 23, 2012
  */
@@ -98,8 +96,8 @@ public class XJDFSheetOptimizeTest extends JDFTestCaseBase
 	private JDFConvertingConfig convertingConfig;
 
 	/**
-	 * 
-	 * 
+	 *
+	 *
 	 */
 	@Test
 	public void testSimple()
@@ -111,24 +109,25 @@ public class XJDFSheetOptimizeTest extends JDFTestCaseBase
 			e.setAttribute("PageDimension", "333 222");
 		}
 		xjdfHelper.getRoot().setXMLComment("most trivial list of single page documents - e.g. business cards");
-		writeTest(xjdfHelper.getRoot(), "SimpleGangIn.xjdf", true);
+		writeTest(xjdfHelper, "SimpleGangIn.xjdf");
 		for (int i = 0; i < 6; i++)
 		{
-			JDFAttributeMap partMap = new JDFAttributeMap("BinderySignatureName", "BS" + i);
+			JDFAttributeMap partMap = new JDFAttributeMap("BinderySignatureID", "BS" + i);
 			String sn = "S" + (i / 3);
 			partMap.put("SheetName", sn);
 			ResourceHelper phLO = layout.appendPartition(partMap, true);
 			phLO.setAmount(1000, partMap, true);
 			JDFLayout lo = (JDFLayout) phLO.getCreateResource();
-			JDFBinderySignature bs = (JDFBinderySignature) lo.appendElement(ElementName.BINDERYSIGNATURE);
-			bs.appendElement(ElementName.POSITION);
+			SetHelper sh = xjdfHelper.getCreateResourceSet(ElementName.BINDERYSIGNATURE, EnumUsage.Input);
+			sh.getCreatePartition(new JDFAttributeMap(XJDFConstants.BinderySignatureID, "BS" + i), true).getResource();
+			lo.appendElement(ElementName.POSITION).setAttribute(XJDFConstants.BinderySignatureID, "BS" + i);
 		}
-		writeTest(xjdfHelper.getRoot(), "SimpleGangOut.xjdf", true);
+		writeTest(xjdfHelper, "SimpleGangOut.xjdf");
 	}
 
 	/**
-	 * 
-	 * 
+	 *
+	 *
 	 */
 	@Test
 	public void testLayout()
@@ -136,14 +135,13 @@ public class XJDFSheetOptimizeTest extends JDFTestCaseBase
 		for (int i = 0; i < 6; i++)
 		{
 			addGang();
-			addGangLayout();
 		}
-		writeTest(xjdfHelper.getRoot(), "SimpleGang.xjdf", true);
+		writeTest(xjdfHelper, "SimpleGang.xjdf");
 	}
 
 	/**
-	 * 
-	 * 
+	 *
+	 *
 	 */
 	@Test
 	public void testOutputLayout()
@@ -152,52 +150,41 @@ public class XJDFSheetOptimizeTest extends JDFTestCaseBase
 		{
 			for (int j = 0; j < 2; j++)
 			{
+				addGang();
 				addOutLayout(i, j);
 			}
 		}
-		writeTest(xjdfHelper.getRoot(), "GangLayout.xjdf", true);
+		writeTest(xjdfHelper, "GangLayout.xjdf");
 	}
 
 	/**
-	 *  
+	 *
 	 * @param i
 	 * @param j
 	 */
 	private void addOutLayout(int i, int j)
 	{
 		JDFAttributeMap partMap = new JDFAttributeMap("SheetName", "Sheet1");
-		partMap.put(EnumPartIDKey.BinderySignatureName, "BS_" + i + "_" + j);
+		String bsIJ = "BS_" + i + "_" + j;
+		partMap.put(XJDFConstants.BinderySignatureID, bsIJ);
 		JDFLayout lo = (JDFLayout) layout.getCreatePartition(partMap, true).getResource();
-		JDFBinderySignature bs = (JDFBinderySignature) lo.appendElement(ElementName.BINDERYSIGNATURE);
-		JDFPosition p = JDFPosition.createPosition(bs, i, j, 2, 2);
+		SetHelper sh = xjdfHelper.getCreateResourceSet(ElementName.BINDERYSIGNATURE, EnumUsage.Input);
+		sh.getCreatePartition(new JDFAttributeMap(XJDFConstants.BinderySignatureID, bsIJ), true).getResource();
+		JDFPosition p = JDFPosition.createPosition(lo, i, j, 2, 2);
+		p.setAttribute(XJDFConstants.BinderySignatureID, bsIJ);
 		assertNotNull(p);
 	}
 
 	/**
-	 *  
-	 *  
+	 *
+	 *
 	 */
 	private KElement addGang()
 	{
-		KElement gang = sheetOptimizingParams.appendElement("GangElement");
-		gang.setAttribute("ID", "Gang_" + gang.numSiblingElements("GangElement", null));
-		gang.setAttribute("OrderedQuantity", 1000, null);
+		KElement gang = sheetOptimizingParams.appendElement(ElementName.GANGELEMENT);
+		gang.setAttribute(AttributeName.GANGELEMENTID, "Gang_" + gang.numSiblingElements(ElementName.GANGELEMENT, null));
+		gang.setAttribute("MinQuantity", 1000, null);
 		return gang;
-	}
-
-	/**
-	 *  
-	 *  
-	 */
-	private KElement addGangLayout()
-	{
-		KElement gangelem = sheetOptimizingParams.getElement("GangElement", null, -1);
-		KElement lo = gangelem.appendElement("Layout");
-		lo.setAttribute("GangID", "Gang_" + gangelem.getID());
-		lo.setAttribute(AttributeName.WORKSTYLE, EnumWorkStyle.Perfecting.getName());
-		JDFBinderySignature bs = (JDFBinderySignature) lo.appendElement(ElementName.BINDERYSIGNATURE);
-		bs.setFoldCatalog("F2-1");
-		return lo;
 	}
 
 	/**
@@ -226,13 +213,11 @@ public class XJDFSheetOptimizeTest extends JDFTestCaseBase
 		ResourceHelper hpSheetOptim = hsSheetOptim.getCreatePartition(0, true);
 		sheetOptimizingParams = hpSheetOptim.getCreateResource();
 
-		SetHelper hsCC = xjdfHelper.getCreateResourceSet(ElementName.CONVERTINGCONFIG, EnumUsage.Input);
-		ResourceHelper hpCC = hsCC.getCreatePartition(0, true);
-		convertingConfig = (JDFConvertingConfig) hpCC.getCreateResource();
-		convertingConfig.setSheetHeight(700 * 72 / 2.54, 700 * 72 / 2.54);
-		convertingConfig.setSheetWidth(1000 * 72 / 2.54, 1000 * 72 / 2.54);
-
-		sheetOptimizingParams.setAttribute(ElementName.CONVERTINGCONFIG + "Ref", hpCC.getID());
+		convertingConfig = (JDFConvertingConfig) sheetOptimizingParams.appendElement(ElementName.CONVERTINGCONFIG);
+		convertingConfig.setAttribute(AttributeName.SHEETHEIGHT + "Max", 700 * 72 / 2.54, null);
+		convertingConfig.setAttribute(AttributeName.SHEETHEIGHT + "Min", 700 * 72 / 2.54, null);
+		convertingConfig.setAttribute(AttributeName.SHEETWIDTH + "Max", 1000 * 72 / 2.54, null);
+		convertingConfig.setAttribute(AttributeName.SHEETWIDTH + "Min", 1000 * 72 / 2.54, null);
 
 	}
 
