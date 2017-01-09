bcfd4f17a2b27501bb84e2a2584990cc5d1f0a23
Rainer Prosi <rainer.prosi@heidelberg.com>
2017-01-09 15:13:18 +0100

    hot folder improvements

--- src/main/java/org/cip4/jdflib/util/hotfolder/StorageHotFolderListener.java
+++ src/main/java/org/cip4/jdflib/util/hotfolder/StorageHotFolderListener.java
@@ -1,7 +1,7 @@
 /**
  * The CIP4 Software License, Version 1.0
  *
- * Copyright (c) 2001-2016 The International Cooperation for the Integration of 
+ * Copyright (c) 2001-2017 The International Cooperation for the Integration of
  * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
  * reserved.
  *
@@ -331,7 +331,7 @@ class StorageHotFolderListener implements HotFolderListener
 	 * @param hotFile
 	 * @return
 	 */
-	File getAuxDir(File hotFile)
+	public static File getAuxDir(File hotFile)
 	{
 		if (hotFile == null)
 		{
