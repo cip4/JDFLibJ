19eb82172b768e4d77f9d75c328d28d2fbc6fd6b
Rainer Prosi <rainer.prosi@heidelberg.com>
2017-01-29 23:06:41 +0100

    cleanup

--- src/main/java/org/cip4/jdflib/extensions/xjdfwalker/xjdftojdf/WalkContent.java
+++ src/main/java/org/cip4/jdflib/extensions/xjdfwalker/xjdftojdf/WalkContent.java
@@ -1,8 +1,8 @@
 /**
  * The CIP4 Software License, Version 1.0
  *
- * Copyright (c) 2001-2015 The International Cooperation for the Integration of 
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
+ * Copyright (c) 2001-2017 The International Cooperation for the Integration of
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -10,7 +10,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -18,17 +18,17 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
- *        The International Cooperation for the Integration of 
+ *        The International Cooperation for the Integration of
  *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of 
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of
  *    Processes in  Prepress, Press and Postpress" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact info@cip4.org.
  *
  * 5. Products derived from this software may not be called "CIP4",
@@ -54,22 +54,24 @@
  * ====================================================================
  *
  * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration 
+ * individuals on behalf of the The International Cooperation for the Integration
  * of Processes in Prepress, Press and Postpress and was
- * originally based on software 
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG 
- * copyright (c) 1999-2001, Agfa-Gevaert N.V. 
- *  
- * For more information on The International Cooperation for the 
+ * originally based on software
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the
  * Integration of Processes in  Prepress, Press and Postpress , please see
  * <http://www.cip4.org/>.
- *  
- * 
+ *
+ *
  */
 package org.cip4.jdflib.extensions.xjdfwalker.xjdftojdf;
 
 import org.cip4.jdflib.core.AttributeName;
 import org.cip4.jdflib.core.KElement;
+import org.cip4.jdflib.core.VString;
+import org.cip4.jdflib.extensions.XJDFConstants;
 
 /**
  * continue walking on these without copying e
@@ -78,27 +80,11 @@ import org.cip4.jdflib.core.KElement;
 public class WalkContent extends WalkResource
 {
 	/**
-	 * 
+	 *
 	 */
 	public WalkContent()
 	{
 		super();
-	}
-
-	/**
-	 * @param e
-	 * @return the created resource
-	 */
-	@Override
-	public KElement walk(final KElement e, final KElement trackElem)
-	{
-		e.renameAttribute(AttributeName.PAGENUMBER, AttributeName.PAGEINDEX, null, null);
-		e.renameAttribute("ContentStatus", AttributeName.PAGESTATUS, null, null);
-		e.removeAttribute(AttributeName.ID);
-		e.removeAttribute(AttributeName.PARTIDKEYS);
-		e.removeAttribute(AttributeName.STATUS);
-		e.removeAttribute("ContentType");
-		return super.walk(e, trackElem);
 	}
 
 	/**
@@ -109,7 +95,31 @@ public class WalkContent extends WalkResource
 	@Override
 	public boolean matches(final KElement toCheck)
 	{
-		return "Content".equals(toCheck.getLocalName());
+		return XJDFConstants.Content.equals(toCheck.getLocalName());
+	}
+
+	/**
+	 * @see org.cip4.jdflib.extensions.xjdfwalker.xjdftojdf.WalkXElement#updateAttributes(org.cip4.jdflib.core.KElement)
+	 */
+	@Override
+	protected void updateAttributes(KElement elem)
+	{
+		elem.renameAttribute(AttributeName.PAGENUMBER, AttributeName.PAGEINDEX, null, null);
+		elem.renameAttribute("ContentStatus", AttributeName.PAGESTATUS, null, null);
+		elem.removeAttribute(AttributeName.ID);
+		elem.removeAttribute(AttributeName.PARTIDKEYS);
+		elem.removeAttribute(AttributeName.STATUS);
+		elem.removeAttribute("ContentType"); // TODO Auto-generated method stub
+		super.updateAttributes(elem);
+	}
+
+	/**
+	 * @see org.cip4.jdflib.elementwalker.BaseWalker#getElementNames()
+	 */
+	@Override
+	public VString getElementNames()
+	{
+		return VString.getVString(XJDFConstants.Content, null);
 	}
 
 }
