b9fc7c1cc766bfb1c096404a6a427db3d4ea0c55
Rainer Prosi <rainer.prosi@heidelberg.com>
2018-02-24 23:52:46 +0100

    added quality control example

--- src/test/java/org/cip4/jdflib/extensions/examples/XJDFAuditTest.java
+++ src/test/java/org/cip4/jdflib/extensions/examples/XJDFAuditTest.java
@@ -1,68 +1,36 @@
 /**
  * The CIP4 Software License, Version 1.0
  *
- * Copyright (c) 2001-2017 The International Cooperation for the Integration of
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights
- * reserved.
+ * Copyright (c) 2001-2018 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
  *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        The International Cooperation for the Integration of
- *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
+ * 3. The end-user documentation included with the redistribution, if any, must include the following acknowledgment: "This product includes software developed by the The International Cooperation for
+ * the Integration of Processes in Prepress, Press and Postpress (www.cip4.org)" Alternately, this acknowledgment may appear in the software itself, if and wherever such third-party acknowledgments
+ * normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of
- *    Processes in  Prepress, Press and Postpress" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact info@cip4.org.
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of Processes in Prepress, Press and Postpress" must not be used to endorse or promote products derived from this software
+ * without prior written permission. For written permission, please contact info@cip4.org.
  *
- * 5. Products derived from this software may not be called "CIP4",
- *    nor may "CIP4" appear in their name, without prior written
- *    permission of the CIP4 organization
+ * 5. Products derived from this software may not be called "CIP4", nor may "CIP4" appear in their name, without prior written permission of the CIP4 organization
  *
- * Usage of this software in commercial products is subject to restrictions. For
- * details please consult info@cip4.org.
+ * Usage of this software in commercial products is subject to restrictions. For details please consult info@cip4.org.
  *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR
- * THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE. ====================================================================
  *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration
- * of Processes in Prepress, Press and Postpress and was
- * originally based on software
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
- * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ * This software consists of voluntary contributions made by many individuals on behalf of the The International Cooperation for the Integration of Processes in Prepress, Press and Postpress and was
+ * originally based on software copyright (c) 1999-2001, Heidelberger Druckmaschinen AG copyright (c) 1999-2001, Agfa-Gevaert N.V.
  *
- * For more information on The International Cooperation for the
- * Integration of Processes in  Prepress, Press and Postpress , please see
- * <http://www.cip4.org/>.
+ * For more information on The International Cooperation for the Integration of Processes in Prepress, Press and Postpress , please see <http://www.cip4.org/>.
  *
  *
  */
@@ -74,6 +42,7 @@ import org.cip4.jdflib.core.AttributeName;
 import org.cip4.jdflib.core.ElementName;
 import org.cip4.jdflib.core.JDFResourceLink.EnumUsage;
 import org.cip4.jdflib.core.KElement;
+import org.cip4.jdflib.datatypes.JDFAttributeMap;
 import org.cip4.jdflib.extensions.AuditPoolHelper;
 import org.cip4.jdflib.extensions.MessageResourceHelper;
 import org.cip4.jdflib.extensions.ResourceHelper;
@@ -83,6 +52,8 @@ import org.cip4.jdflib.extensions.XJDFHelper;
 import org.cip4.jdflib.node.JDFNode.EnumType;
 import org.cip4.jdflib.resource.process.JDFComponent;
 import org.cip4.jdflib.resource.process.JDFMedia;
+import org.cip4.jdflib.resource.process.JDFQualityControlResult;
+import org.cip4.jdflib.util.JDFDate;
 import org.junit.Test;
 
 /**
@@ -98,30 +69,30 @@ public class XJDFAuditTest extends JDFTestCaseBase
 	@Test
 	public void testResourceAudit()
 	{
-		XJDFHelper xjdfHelper = new XJDFHelper("PaperAudit", null, null);
+		final XJDFHelper xjdfHelper = new XJDFHelper("PaperAudit", null, null);
 		xjdfHelper.setTypes(EnumType.ConventionalPrinting.getName());
-		SetHelper shMedia = xjdfHelper.getCreateSet(XJDFConstants.Resource, ElementName.MEDIA, null);
-		SetHelper shComp = xjdfHelper.getCreateSet(XJDFConstants.Resource, ElementName.COMPONENT, EnumUsage.Input);
+		final SetHelper shMedia = xjdfHelper.getCreateSet(XJDFConstants.Resource, ElementName.MEDIA, null);
+		final SetHelper shComp = xjdfHelper.getCreateSet(XJDFConstants.Resource, ElementName.COMPONENT, EnumUsage.Input);
 
-		ResourceHelper rhM = shMedia.appendPartition(AttributeName.SHEETNAME, "S1", true);
+		final ResourceHelper rhM = shMedia.appendPartition(AttributeName.SHEETNAME, "S1", true);
 		ResourceHelper rhC = shComp.appendPartition(AttributeName.SHEETNAME, "S1", true);
 		rhC.setAmount(400, null, true);
 		JDFMedia m = (JDFMedia) rhM.getResource();
 		m.setWeight(80);
 		m.setMediaType(EnumMediaType.Paper);
-		JDFComponent comp = (JDFComponent) rhC.getResource();
+		final JDFComponent comp = (JDFComponent) rhC.getResource();
 		comp.setAttribute("MediaRef", rhM.ensureID());
 
 		xjdfHelper.writeToFile(sm_dirTestDataTemp + "/xjdf/PaperAudit.xjdf");
 
-		AuditPoolHelper ah = xjdfHelper.getCreateAuditPool();
-		MessageResourceHelper crh = ah.getCreateMessageResourceHelper(shComp);
-		SetHelper aSet = crh.getSet();
+		final AuditPoolHelper ah = xjdfHelper.getCreateAuditPool();
+		final MessageResourceHelper crh = ah.getCreateMessageResourceHelper(shComp);
+		final SetHelper aSet = crh.getSet();
 		rhC = aSet.getCreatePartition(AttributeName.SHEETNAME, "S1", true);
 		rhC.setAmount(400, null, true);
 		rhC.setAmount(21, null, false);
 
-		MessageResourceHelper mrh = ah.getCreateMessageResourceHelper(shMedia);
+		final MessageResourceHelper mrh = ah.getCreateMessageResourceHelper(shMedia);
 		m = (JDFMedia) mrh.getSet().getCreatePartition(null, true).getResource();
 		m.setWeight(90);
 		m.setMediaType(EnumMediaType.Paper);
@@ -130,6 +101,50 @@ public class XJDFAuditTest extends JDFTestCaseBase
 		setSnippet(xjdfHelper, true);
 		setSnippet(xjdfHelper.getSet(ElementName.NODEINFO, 0), false);
 		writeTest(xjdfHelper, "structure/PaperAuditActual.xjdf");
+	}
+
+	/**
+	*
+	*
+	*/
+	@Test
+	public final void testQualityControl()
+	{
+		final XJDFHelper xjdfHelper = new XJDFHelper("QualityControlExample", null, null);
+		xjdfHelper.addType(EnumType.ConventionalPrinting.getName(), 0);
+		xjdfHelper.addType(EnumType.QualityControl.getName(), -1);
+
+		xjdfHelper.getCreateSet(ElementName.COMPONENT, EnumUsage.Output);
+		final SetHelper qqp = xjdfHelper.getCreateSet(ElementName.QUALITYCONTROLPARAMS, EnumUsage.Input);
+		final SetHelper qqr = xjdfHelper.getCreateSet(ElementName.QUALITYCONTROLRESULT, EnumUsage.Output);
+		final ResourceHelper qpr = qqp.appendPartition(null, true);
+		qpr.getRoot().appendElement("cc:CxF", "http://colorexchangeformat.com/CxF3-core").setText("CxF requirement data is in here");
+		qpr.getResource().setAttribute(AttributeName.SAMPLEINTERVAL, "42");
+
+		final AuditPoolHelper ah = xjdfHelper.getCreateAuditPool();
+		final MessageResourceHelper auditQP = ah.getCreateMessageResourceHelper(qqr);
+		final SetHelper auditSet = auditQP.getSet();
+		int meas = 0;
+		for (int sheet = 1; sheet < 3; sheet++)
+		{
+			final JDFAttributeMap mA = new JDFAttributeMap(AttributeName.SHEETNAME, "S" + sheet);
+			for (int j = 0; j < 2; j++)
+			{
+				meas++;
+				mA.put(XJDFConstants.QualityMeasurement, "M" + meas);
+				final ResourceHelper auditRes = auditSet.getCreatePartition(mA, true);
+				final JDFQualityControlResult res = (JDFQualityControlResult) auditRes.getResource();
+				res.setPassed(200);
+				res.setFailed(0);
+				res.setAttribute(AttributeName.START, new JDFDate().addOffset(0, 0, meas * 2, 0).getDateTimeISO());
+				res.setAttribute(AttributeName.END, new JDFDate().addOffset(0, 0, meas * 2 + 1, 0).getDateTimeISO());
+				auditRes.getRoot().appendElement("cc:CxF", "http://colorexchangeformat.com/CxF3-core").setText("CxF measurement data is in here");
+			}
+		}
+
+		cleanSnippets(xjdfHelper);
+		writeRoundTripX(xjdfHelper.getRoot(), "QualityControlAuditCxF");
+
 	}
 
 	/**
