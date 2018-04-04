628e65123ad747b4ef9d2fef06da34035a3d76fb
Rainer Prosi <rainer.prosi@heidelberg.com>
2018-04-04 14:35:02 +0200

    xjmf conversion for setgang

--- src/main/java/org/cip4/jdflib/extensions/xjdfwalker/jdftoxjdf/WalkModifyQueueEntry.java
+++ src/main/java/org/cip4/jdflib/extensions/xjdfwalker/jdftoxjdf/WalkModifyQueueEntry.java
@@ -1,68 +1,36 @@
 /**
  * The CIP4 Software License, Version 1.0
  *
- * Copyright (c) 2001-2017 The International Cooperation for the Integration of
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights
- * reserved.
+ * Copyright (c) 2001-2018 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
  *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        The International Cooperation for the Integration of
- *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
+ * 3. The end-user documentation included with the redistribution, if any, must include the following acknowledgment: "This product includes software developed by the The International Cooperation for
+ * the Integration of Processes in Prepress, Press and Postpress (www.cip4.org)" Alternately, this acknowledgment may appear in the software itself, if and wherever such third-party acknowledgments
+ * normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of
- *    Processes in  Prepress, Press and Postpress" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact info@cip4.org.
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of Processes in Prepress, Press and Postpress" must not be used to endorse or promote products derived from this software
+ * without prior written permission. For written permission, please contact info@cip4.org.
  *
- * 5. Products derived from this software may not be called "CIP4",
- *    nor may "CIP4" appear in their name, without prior written
- *    permission of the CIP4 organization
+ * 5. Products derived from this software may not be called "CIP4", nor may "CIP4" appear in their name, without prior written permission of the CIP4 organization
  *
- * Usage of this software in commercial products is subject to restrictions. For
- * details please consult info@cip4.org.
+ * Usage of this software in commercial products is subject to restrictions. For details please consult info@cip4.org.
  *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR
- * THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE. ====================================================================
  *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration
- * of Processes in Prepress, Press and Postpress and was
- * originally based on software
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
- * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ * This software consists of voluntary contributions made by many individuals on behalf of the The International Cooperation for the Integration of Processes in Prepress, Press and Postpress and was
+ * originally based on software copyright (c) 1999-2001, Heidelberger Druckmaschinen AG copyright (c) 1999-2001, Agfa-Gevaert N.V.
  *
- * For more information on The International Cooperation for the
- * Integration of Processes in  Prepress, Press and Postpress , please see
- * <http://www.cip4.org/>.
+ * For more information on The International Cooperation for the Integration of Processes in Prepress, Press and Postpress , please see <http://www.cip4.org/>.
  *
  *
  */
@@ -81,7 +49,7 @@ import org.cip4.jdflib.util.StringUtil;
 
 /**
  * @author Rainer Prosi, Heidelberger Druckmaschinen <br/>
- * walker for JMF mesaages
+ *         walker for JMF mesaages
  */
 public class WalkModifyQueueEntry extends WalkMessage
 {
@@ -104,15 +72,15 @@ public class WalkModifyQueueEntry extends WalkMessage
 		m = super.makeTypesafe(m);
 		if ((m instanceof JDFQuery) || (m instanceof JDFCommand))
 		{
-			String id = m.getID();
-			String originalType = super.getMessageType(m);
-			String operation = StringUtil.leftStr(originalType, -10); //-10 = queueentry.size()
+			final String id = m.getID();
+			final String originalType = super.getMessageType(m);
+			final String operation = StringUtil.leftStr(originalType, -10); // -10 = queueentry.size()
 			XJMFTypeMap.getMap().put(id, originalType);
-			KElement modifyParams = m.getCreateElement("ModifyQueueEntryParams", null, 0);
+			final KElement modifyParams = m.getCreateElement("ModifyQueueEntryParams", null, 0);
 			modifyParams.setAttribute(AttributeName.OPERATION, operation);
-			String oldParams = originalType + "Params";
-			JDFQueueEntryDef queueEntryDef = (JDFQueueEntryDef) m.getElement(ElementName.QUEUEENTRYDEF, null, 0);
-			String qeid = queueEntryDef == null ? null : queueEntryDef.getQueueEntryID();
+			final String oldParams = originalType + "Params";
+			final JDFQueueEntryDef queueEntryDef = (JDFQueueEntryDef) m.getElement(ElementName.QUEUEENTRYDEF, null, 0);
+			final String qeid = queueEntryDef == null ? null : queueEntryDef.getQueueEntryID();
 			if (qeid != null)
 			{
 				modifyParams.setXPathAttribute("QueueFilter/@QueueEntryIDs", qeid);
@@ -120,12 +88,12 @@ public class WalkModifyQueueEntry extends WalkMessage
 			}
 			else
 			{
-				KElement op = m.getElement(oldParams);
+				final KElement op = m.getElement(oldParams);
 				if (op != null)
 				{
 					if ("Abort".equals(operation))
 					{
-						String endstate = op.getNonEmpty(AttributeName.ENDSTATUS);
+						final String endstate = op.getNonEmpty(AttributeName.ENDSTATUS);
 						if (EnumNodeStatus.Completed.getName().equals(endstate))
 						{
 							modifyParams.setAttribute(AttributeName.OPERATION, "Complete");
@@ -144,7 +112,7 @@ public class WalkModifyQueueEntry extends WalkMessage
 	 * @see org.cip4.jdflib.extensions.XJDF20.WalkMessage#getMessageType(org.cip4.jdflib.jmf.JDFMessage)
 	 */
 	@Override
-	String getMessageType(JDFMessage m)
+	String getMessageType(final JDFMessage m)
 	{
 		return "ModifyQueueEntry";
 	}
@@ -153,7 +121,7 @@ public class WalkModifyQueueEntry extends WalkMessage
 	 * @see org.cip4.jdflib.extensions.XJDF20.WalkMessage#matches(org.cip4.jdflib.core.KElement)
 	 */
 	@Override
-	public boolean matches(KElement toCheck)
+	public boolean matches(final KElement toCheck)
 	{
 		return !jdfToXJDF.isRetainAll() && super.matches(toCheck) && isQueueControl(toCheck.getAttribute(AttributeName.TYPE));
 	}
@@ -163,9 +131,9 @@ public class WalkModifyQueueEntry extends WalkMessage
 	 * @param type
 	 * @return
 	 */
-	boolean isQueueControl(String type)
+	boolean isQueueControl(final String type)
 	{
-		return StringUtil.hasToken("AbortQueueEntry,HoldQueueEntry,RemoveQueueEntry,ResumeQueueEntry,SetQueueEntryPosition,SetQueueEntryPriority,SuspendQueueEntry", type, ",", 0);
+		return StringUtil.hasToken("AbortQueueEntry,HoldQueueEntry,RemoveQueueEntry,ResumeQueueEntry,SetGangQueueEntry,SetQueueEntryPosition,SetQueueEntryPriority,SuspendQueueEntry", type, ",", 0);
 	}
 
 }
