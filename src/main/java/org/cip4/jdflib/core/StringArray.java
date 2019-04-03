4659184d1c5d61893f0cabf9cee75f2730ad7cd6
Rainer Prosi <rainer.prosi@heidelberg.com>
2019-04-03 13:14:35 +0200

    added higher performance StringArray

---
+++ src/main/java/org/cip4/jdflib/core/StringArray.java
@@ -0,0 +1,388 @@
+/*
+ *
+ * The CIP4 Software License, Version 1.0
+ *
+ *
+ * Copyright (c) 2001-2019 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if any, must include the following acknowledgment: "This product includes software developed by the The International Cooperation for
+ * the Integration of Processes in Prepress, Press and Postpress (www.cip4.org)" Alternately, this acknowledgment may appear in the software itself, if and wherever such third-party acknowledgments
+ * normally appear.
+ *
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of Processes in Prepress, Press and Postpress" must not be used to endorse or promote products derived from this software
+ * without prior written permission. For written permission, please contact info@cip4.org.
+ *
+ * 5. Products derived from this software may not be called "CIP4", nor may "CIP4" appear in their name, without prior written permission of the CIP4 organization
+ *
+ * Usage of this software in commercial products is subject to restrictions. For details please consult info@cip4.org.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE. ====================================================================
+ *
+ * This software consists of voluntary contributions made by many individuals on behalf of the The International Cooperation for the Integration of Processes in Prepress, Press and Postpress and was
+ * originally based on software copyright (c) 1999-2001, Heidelberger Druckmaschinen AG copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the Integration of Processes in Prepress, Press and Postpress , please see <http://www.cip4.org/>.
+ *
+ *
+ */
+/**
+ *
+ * Copyright (c) 2001 Heidelberger Druckmaschinen AG, All Rights Reserved.
+ *
+ * vString.java
+ *
+ * Last changes
+ *
+ */
+package org.cip4.jdflib.core;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.cip4.jdflib.util.ContainerUtil;
+import org.cip4.jdflib.util.StringUtil;
+
+/**
+ *
+ */
+public class StringArray extends ArrayList<String>
+{
+	private static final long serialVersionUID = 1L;
+	/**
+	 * the empty VString
+	 */
+	final public static StringArray emptyVector = new StringArray();
+
+	/**
+	 * are we null or empty or contain only an empty JDFAttributeMap
+	 *
+	 * @param v
+	 * @return
+	 */
+	public static boolean isEmpty(final Collection<String> v)
+	{
+		return v == null || v.isEmpty() || v.size() == 1 && StringUtil.isEmpty(v.iterator().next());
+	}
+
+	/**
+	 * constructor
+	 */
+	public StringArray()
+	{
+		super();
+	}
+
+	/**
+	 * constructor
+	 *
+	 * @param m
+	 */
+	public StringArray(final Collection<String> m)
+	{
+		super();
+		if (m != null)
+		{
+			addAll(m);
+		}
+	}
+
+	/**
+	 * convenience - constructs a VString by tokenizing a string
+	 *
+	 * @param strIn the string to tokenize by blank
+	 *
+	 */
+	public StringArray(final String strIn)
+	{
+		this(strIn, null);
+	}
+
+	/**
+	 * simple static factory - null if strIn is null or empty
+	 *
+	 * @param strIn
+	 * @param strSep
+	 * @return
+	 */
+	public static StringArray getVString(final String strIn, final String strSep)
+	{
+		return StringUtil.getNonEmpty(strIn) == null ? null : new StringArray(strIn, strSep);
+	}
+
+	/**
+	 *
+	 * constructs a VString by tokenizing a string
+	 *
+	 * @param strIn the string to tokenize
+	 * @param strSep the separator character
+	 */
+	public StringArray(final String strIn, String strSep)
+	{
+		super();
+		if (!StringUtil.isEmpty(strIn))
+		{
+			if (strSep == null)
+			{
+				strSep = JDFCoreConstants.BLANK;
+			}
+			if (strSep.length() == 1)
+			{
+				// performance boost...
+				if (strIn.indexOf(strSep) < 0)
+				{
+					add(strIn);
+					return;
+				}
+			}
+			final StringTokenizer sToken = new StringTokenizer(strIn, strSep);
+			while (sToken.hasMoreTokens())
+			{
+				add(sToken.nextToken());
+			}
+		}
+	}
+
+	/**
+	 * creates a VString from an array of Strings
+	 *
+	 * @param a the array
+	 */
+	public StringArray(final String[] a)
+	{
+		super(a == null ? 0 : a.length);
+		final int l = a == null ? 0 : a.length;
+		for (int i = 0; i < l; i++)
+		{
+			add(a[i]);
+		}
+	}
+
+	/**
+	 * @param index the index which may be negative to count backwards
+	 * @return the string at index
+	 */
+	@Override
+	public String get(int index)
+	{
+		if (index < 0)
+		{
+			index += size();
+		}
+		return index >= 0 && index < size() ? super.get(index) : null;
+	}
+
+	/**
+	 * AppendUnique - append a string but ignore multiple entries
+	 *
+	 * @param string the string to append, if null nothing is added
+	 */
+	public void appendUnique(final String string)
+	{
+		if (string != null && !contains(string))
+		{
+			add(string);
+		}
+	}
+
+	/**
+	 * AppendUnique - append a vector but ignore multiple entries
+	 *
+	 * @param v the vector to append, if null nothing is added
+	 */
+	public void appendUnique(final Collection<String> v)
+	{
+		if (!StringArray.isEmpty(v))
+		{
+			final int size = v.size();
+			if (size == 1) // speedup for single append
+			{
+				final String theOther = v.iterator().next();
+				if (!contains(theOther))
+				{
+					add(theOther);
+				}
+			}
+			else
+			{
+				addAll(v);
+				unify();
+			}
+		}
+	}
+
+	/**
+	 * serialize to a string
+	 *
+	 * @param sep separator between strings
+	 * @param front string before the first entry
+	 * @param back string after the last entry
+	 *
+	 * @return a tokenized string
+	 */
+	public String getString(final String sep, final String front, final String back)
+	{
+		return StringUtil.setvString(this, sep, front, back);
+	}
+
+	/**
+	 * serialize to a string
+	 *
+	 * @param sep separator between strings
+	 * @param front string before the first entry
+	 * @param back string after the last entry
+	 *
+	 * @return a tokenized string
+	 */
+	public String getString()
+	{
+		return StringUtil.setvString(this, JDFConstants.BLANK, null, null);
+	}
+
+	/**
+	 * unify - make VString unique, retaining initial order
+	 */
+	public void unify()
+	{
+		ContainerUtil.unify(this);
+	}
+
+	/**
+	 * get a string from <code>this</code>
+	 *
+	 * @param s the String you are looking for
+	 * @return the String if found or null if <code>this</code> does not contain s
+	 */
+	public String get(final String s)
+	{
+		if (contains(s))
+		{
+			final int i = indexOf(s);
+			return get(i);
+		}
+
+		return null;
+	}
+
+	/**
+	 * gets a set with all entries of the VString<br/>
+	 * note that the set retains ordering (LinkedHashSet)
+	 *
+	 * @return the set corresponding to this
+	 */
+	public Set<String> getSet()
+	{
+		final HashSet<String> set = new LinkedHashSet<>();
+		final Iterator<String> it = iterator();
+		while (it.hasNext())
+		{
+			set.add(it.next());
+		}
+
+		return set;
+	}
+
+	/**
+	 * appends all strings of an array to <code>this</code>
+	 *
+	 * @param strings the array of strings to append to <code>this</code>
+	 */
+	public void addAll(final String[] strings)
+	{
+		ensureCapacity(size() + strings.length);
+		for (final String string : strings)
+		{
+			add(string);
+		}
+	}
+
+	/**
+	 * checks whether at least one of a given vector of strings is contained in <code>this</code>
+	 *
+	 * @param others the VSTring of values to test
+	 * @return true if at least one String in other is in <code>this</code>
+	 */
+	public boolean containsAny(final StringArray others)
+	{
+		if (isEmpty(others))
+		{
+			return false;
+		}
+		for (final String other : others)
+		{
+			if (contains(other))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * vector of strings that is contained in <code>this</code>
+	 *
+	 * @param others the VString of values to test
+	 * @return true if at least one String in other is in <code>this</code>
+	 */
+	public StringArray getOverlapping(final Collection<String> others)
+	{
+		if (others == null)
+		{
+			return null;
+		}
+		final StringArray ret = new StringArray();
+		for (final String s : this)
+		{
+			if (others.contains(s))
+			{
+				ret.add(s);
+			}
+		}
+		return ret.size() > 0 ? ret : null;
+	}
+
+	/**
+	 * remove but also implementing the usual neg number syntax
+	 *
+	 * @param index if 0, or positive count from front, else if negative from back
+	 * @see java.util.Vector#remove(int)
+	 */
+	@Override
+	public String remove(int index)
+	{
+		if (index < 0)
+		{
+			index += size();
+		}
+		return super.remove(index);
+	}
+
+	/**
+	 * add a string if it is not empty
+	 *
+	 * @param text
+	 */
+	public void addNonEmpty(final String text)
+	{
+		final String t2 = StringUtil.getNonEmpty(text);
+		if (t2 != null)
+			add(t2);
+
+	}
+
+}
---
+++ src/test/java/org/cip4/jdflib/core/StringArrayTest.java
@@ -0,0 +1,440 @@
+/*
+ *
+ * The CIP4 Software License, Version 1.0
+ *
+ *
+ * Copyright (c) 2001-2019 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if any, must include the following acknowledgment: "This product includes software developed by the The International Cooperation for
+ * the Integration of Processes in Prepress, Press and Postpress (www.cip4.org)" Alternately, this acknowledgment may appear in the software itself, if and wherever such third-party acknowledgments
+ * normally appear.
+ *
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of Processes in Prepress, Press and Postpress" must not be used to endorse or promote products derived from this software
+ * without prior written permission. For written permission, please contact info@cip4.org.
+ *
+ * 5. Products derived from this software may not be called "CIP4", nor may "CIP4" appear in their name, without prior written permission of the CIP4 organization
+ *
+ * Usage of this software in commercial products is subject to restrictions. For details please consult info@cip4.org.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE. ====================================================================
+ *
+ * This software consists of voluntary contributions made by many individuals on behalf of the The International Cooperation for the Integration of Processes in Prepress, Press and Postpress and was
+ * originally based on software copyright (c) 1999-2001, Heidelberger Druckmaschinen AG copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the Integration of Processes in Prepress, Press and Postpress , please see <http://www.cip4.org/>.
+ *
+ *
+ */
+package org.cip4.jdflib.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Vector;
+
+import org.cip4.jdflib.JDFTestCaseBase;
+import org.cip4.jdflib.util.StringUtil;
+import org.junit.Test;
+
+/**
+ * @author Rainer Prosi, Heidelberger Druckmaschinen
+ *
+ */
+public class StringArrayTest extends JDFTestCaseBase
+{
+	/**
+	 *
+	 */
+	@Test
+	public void testGetAllString()
+	{
+		final StringArray v = new StringArray();
+		v.appendUnique("a");
+		v.appendUnique("b");
+		v.appendUnique("c");
+		v.appendUnique("c");
+		assertEquals("a b c", StringUtil.setvString(v, " ", null, null), "a b c");
+
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testGet()
+	{
+		final StringArray v = new StringArray();
+		v.add("a");
+		v.add("b");
+		v.add("c");
+		v.add("c");
+		assertEquals("a", v.get(0));
+		assertEquals("c", v.get(3));
+		assertEquals("c", v.get(-2));
+		assertEquals("b", v.get(-3));
+		assertNull("b", v.get(-6));
+		assertNull("b", v.get(7));
+	}
+
+	/**
+	 * @throws Exception
+	 *
+	 */
+	@Test
+	public void testRemove() throws Exception
+	{
+		final StringArray v = new StringArray("a b c", null);
+		assertEquals(v.remove(-1), "c");
+		assertEquals(v.remove(-1), "b");
+		assertEquals(v.size(), 1);
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testContainsAny()
+	{
+		final StringArray v = new StringArray();
+		v.appendUnique("a");
+		v.appendUnique("b");
+		v.appendUnique("c");
+		v.add("c");
+		assertFalse(v.containsAny(null));
+		assertFalse(v.containsAny(new StringArray("d e", " ")));
+		assertTrue(v.containsAny(new StringArray("b e", " ")));
+		assertTrue(v.containsAny(new StringArray("e b", " ")));
+		assertTrue(v.containsAny(new StringArray("g c h", " ")));
+		assertTrue(v.containsAny(v));
+
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testIsEmpty()
+	{
+		assertTrue(StringArray.isEmpty(null));
+		final StringArray v = new StringArray();
+		assertTrue(StringArray.isEmpty(v));
+		v.appendUnique("");
+		assertTrue(StringArray.isEmpty(v));
+		v.set(0, "b");
+		assertFalse(StringArray.isEmpty(v));
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testGetOverlapping()
+	{
+		final StringArray v = new StringArray();
+		v.appendUnique("a");
+		v.appendUnique("b");
+		v.appendUnique("c");
+		v.add("c");
+		assertNull(v.getOverlapping(null));
+		assertNull(v.getOverlapping(new StringArray("d e", " ")));
+		assertEquals(new StringArray("b", null), v.getOverlapping(new StringArray("b e", " ")));
+		assertEquals(new StringArray("c c", null), v.getOverlapping(new StringArray("c", " ")));
+		assertEquals(new StringArray("b c c", null), v.getOverlapping(new StringArray("b c c c", " ")));
+		assertEquals(v, v.getOverlapping(v));
+	}
+
+	/**
+	*
+	*/
+	@Test
+	public void testPerformance()
+	{
+		final long t0 = System.currentTimeMillis();
+		for (int i = 0; i < 10000; i++)
+		{
+			final StringArray v = new StringArray();
+			for (int j = 0; j < 1000; j++)
+			{
+				v.add("" + j);
+			}
+		}
+		final long t1 = System.currentTimeMillis();
+		for (int i = 0; i < 10000; i++)
+		{
+			final ArrayList<String> v = new ArrayList<>();
+			for (int j = 0; j < 1000; j++)
+			{
+				v.add("" + j);
+			}
+		}
+		final long t2 = System.currentTimeMillis();
+		final Vector<String> v = new Vector<>();
+		for (int i = 0; i < 1000; i++)
+		{
+			v.add("" + i);
+		}
+		for (int i = 0; i < 10000; i++)
+		{
+			for (int j = 0; j < 1000; j++)
+			{
+				final String s = v.get(j);
+			}
+		}
+		final long t3 = System.currentTimeMillis();
+		final ArrayList<String> a = new ArrayList<>();
+		for (int i = 0; i < 1000; i++)
+		{
+			a.add("" + i);
+		}
+		for (int i = 0; i < 10000; i++)
+		{
+			for (int j = 0; j < 1000; j++)
+			{
+				final String s = a.get(j);
+			}
+		}
+		final long t4 = System.currentTimeMillis();
+		log.info(("v " + (t1 - t0)));
+		log.info(("a " + (t2 - t1)));
+		log.info(("vg " + (t3 - t2)));
+		log.info(("ag " + (t4 - t3)));
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testSetNull()
+	{
+		final StringArray v = new StringArray();
+		v.add((String) null);
+		v.add("b");
+		v.add("c");
+		assertEquals("b c", StringUtil.setvString(v, " ", null, null), "b c");
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testGetSet()
+	{
+		final StringArray v = new StringArray();
+		v.add("a");
+		v.add("c");
+		v.add("b");
+		final Set<?> s = v.getSet();
+		assertEquals(v.size(), s.size());
+		assertTrue(s.contains("c"));
+
+	}
+
+	// /////////////////////////////////////////////////
+
+	/**
+	 *
+	 */
+	@Test
+	public void testUnify()
+	{
+		final StringArray v = new StringArray();
+		v.add("a");
+		v.add("b");
+		v.add("c");
+		v.add("c");
+		final StringArray w = new StringArray();
+		w.add("c");
+		w.add("b");
+		w.add("a");
+		w.add("a");
+		w.add("d");
+
+		v.unify();
+		assertEquals("a b c", StringUtil.setvString(v, " ", null, null), "a b c");
+		v.appendUnique(w);
+		assertEquals("a b c d", StringUtil.setvString(v, " ", null, null), "a b c d");
+
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testGetString()
+	{
+		final StringArray v = new StringArray();
+		v.add("a");
+		v.add("b");
+		v.add("c");
+		v.add("c");
+		assertEquals("a b c c", v.getString());
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testGetStringABC()
+	{
+		final StringArray v = new StringArray();
+		v.add("a");
+		v.add("b");
+		v.add("c");
+		v.add("c");
+		assertEquals("abcc", v.getString(null, null, null));
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testAddAll()
+	{
+		final StringArray v = new StringArray();
+		v.add("a");
+		v.add("b");
+		v.add("c");
+		v.add("c");
+		final HashSet<String> h = new HashSet<>();
+		h.add("c");
+		h.add("b");
+		h.add("a");
+		h.add("d");
+
+		v.unify();
+		assertEquals("a b c", StringUtil.setvString(v, " ", null, null), "a b c");
+		v.addAll(h);
+		v.unify();
+		assertEquals("a b c d", StringUtil.setvString(v, " ", null, null), "a b c d");
+
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testAddNonEmpty()
+	{
+		final StringArray v = new StringArray();
+		v.addNonEmpty("");
+		assertEquals(0, v.size());
+		v.addNonEmpty(null);
+		assertEquals(0, v.size());
+		v.addNonEmpty(" ");
+		assertEquals(1, v.size());
+	}
+
+	/**
+	*
+	*/
+	@Test
+	public void testAppendUnique()
+	{
+		final StringArray v = new StringArray();
+		v.appendUnique((String) null);
+		assertEquals(0, v.size());
+		v.appendUnique("a");
+		assertEquals(1, v.size());
+		v.appendUnique("b");
+		assertEquals(2, v.size());
+		v.appendUnique("c");
+		assertEquals(3, v.size());
+		v.appendUnique("c");
+		assertEquals(3, v.size());
+	}
+
+	/**
+	*
+	*/
+	@Test
+	public void testAppendUniqueV()
+	{
+		final StringArray v = new StringArray();
+		v.appendUnique((String) null);
+		assertEquals(0, v.size());
+		v.appendUnique("a");
+		assertEquals(1, v.size());
+		v.appendUnique(v);
+		assertEquals(1, v.size());
+		final StringArray v2 = new StringArray("b c");
+		v.appendUnique(v2);
+		assertEquals(3, v.size());
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testConstructEmpty()
+	{
+		StringArray v = new StringArray("", ",");
+		assertEquals(v.size(), 0);
+		v = new StringArray((String) null, null);
+		assertEquals(v.size(), 0);
+		v = new StringArray((String[]) null);
+		assertEquals(v.size(), 0);
+		v = new StringArray((new String[] {}));
+		assertEquals(v.size(), 0);
+		v = new StringArray((StringArray) null);
+		assertEquals(v.size(), 0);
+		v = new StringArray(new StringArray());
+		assertEquals(v.size(), 0);
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testConstructDouble()
+	{
+		StringArray v = new StringArray("a,b,c, ,", ",");
+		assertEquals(v.get(-1), " ");
+		v = new StringArray("a,b,c,,", ",");
+		assertEquals("double tokens are ignored", v.get(-1), "c");
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testFactory()
+	{
+		assertNull(StringArray.getVString(null, null));
+		assertNull(StringArray.getVString("", null));
+		assertEquals(StringArray.getVString("a", null).get(0), "a");
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testSetElementAt()
+	{
+		final StringArray v = new StringArray();
+		v.add("a");
+		v.add("b");
+		v.add("c");
+		v.add("c");
+		v.add("e");
+		v.set(3, "d");
+		assertEquals("a b c d e", StringUtil.setvString(v, " ", null, null));
+
+	}
+
+}
