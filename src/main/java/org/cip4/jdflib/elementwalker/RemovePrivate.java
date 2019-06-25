e7df63c141afd8836d19b6ba6f5a098d53d8fbf6
Rainer Prosi <rainer.prosi@heidelberg.com>
2019-06-25 16:21:07 +0200

    RemovePrivate also zapps namespaces

--- src/main/java/org/cip4/jdflib/elementwalker/RemovePrivate.java
+++ src/main/java/org/cip4/jdflib/elementwalker/RemovePrivate.java
@@ -2,72 +2,40 @@
  * The CIP4 Software License, Version 1.0
  *
  *
- * Copyright (c) 2001-2014 The International Cooperation for the Integration of
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights
- * reserved.
+ * Copyright (c) 2001-2019 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
  *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        The International Cooperation for the Integration of
- *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
- *    Alternately, this acknowledgment mrSubRefay appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
+ * 3. The end-user documentation included with the redistribution, if any, must include the following acknowledgment: "This product includes software developed by the The International Cooperation for
+ * the Integration of Processes in Prepress, Press and Postpress (www.cip4.org)" Alternately, this acknowledgment mrSubRefay appear in the software itself, if and wherever such third-party
+ * acknowledgments normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of
- *    Processes in  Prepress, Press and Postpress" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact info@cip4.org.
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of Processes in Prepress, Press and Postpress" must not be used to endorse or promote products derived from this software
+ * without prior written permission. For written permission, please contact info@cip4.org.
  *
- * 5. Products derived from this software may not be called "CIP4",
- *    nor may "CIP4" appear in their name, without prior writtenrestartProcesses()
- *    permission of the CIP4 organization
+ * 5. Products derived from this software may not be called "CIP4", nor may "CIP4" appear in their name, without prior writtenrestartProcesses() permission of the CIP4 organization
  *
- * Usage of this software in commercial products is subject to restrictions. For
- * details please consult info@cip4.org.
+ * Usage of this software in commercial products is subject to restrictions. For details please consult info@cip4.org.
  *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR
- * THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIrSubRefAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIrSubRefAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. ====================================================================
  *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration
- * of Processes in Prepress, Press and Postpress and was
- * originally based on software restartProcesses()
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
- * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ * This software consists of voluntary contributions made by many individuals on behalf of the The International Cooperation for the Integration of Processes in Prepress, Press and Postpress and was
+ * originally based on software restartProcesses() copyright (c) 1999-2001, Heidelberger Druckmaschinen AG copyright (c) 1999-2001, Agfa-Gevaert N.V.
  *
- * For more information on The International Cooperation for the
- * Integration of Processes in  Prepress, Press and Postpress , please see
- * <http://www.cip4.org/>.
+ * For more information on The International Cooperation for the Integration of Processes in Prepress, Press and Postpress , please see <http://www.cip4.org/>.
  *
  */
 /**
- * 
+ *
  */
 package org.cip4.jdflib.elementwalker;
 
@@ -82,8 +50,8 @@ import org.cip4.jdflib.resource.process.JDFGeneralID;
 
 /**
  * @author Dr. Rainer Prosi, Heidelberger Druckmaschinen AG<br/>
- * removes any private extensions, including prefixed keys in GeneralID elements
- * 
+ *         removes any private extensions, including prefixed keys in GeneralID elements
+ *
  */
 public class RemovePrivate extends BaseElementWalker
 {
@@ -93,7 +61,7 @@ public class RemovePrivate extends BaseElementWalker
 	boolean zappAttributes;
 
 	/**
-	 * 
+	 *
 	 */
 	public RemovePrivate()
 	{
@@ -105,20 +73,21 @@ public class RemovePrivate extends BaseElementWalker
 
 	/**
 	 * add a prefix if never called -all prefixes are zapped
+	 *
 	 * @param prefix
 	 */
-	public void addPrefix(String prefix)
+	public void addPrefix(final String prefix)
 	{
 		if (prefixes == null)
-			prefixes = new HashSet<String>();
+			prefixes = new HashSet<>();
 		prefixes.add(prefix);
 	}
 
 	/**
 	 * the resource walker note the naming convention Walkxxx so that it is automagically instantiated by the super classes
-	 * 
+	 *
 	 * @author prosirai
-	 * 
+	 *
 	 */
 	public class WalkElement extends BaseWalker
 	{
@@ -157,13 +126,20 @@ public class RemovePrivate extends BaseElementWalker
 			}
 			if (unknown != null)
 			{
-				for (String attName : unknown)
+				for (final String attName : unknown)
 				{
 					if (prefixes != null)
 					{
-						String prefix = KElement.xmlnsPrefix(attName);
+						final String prefix = KElement.xmlnsPrefix(attName);
+						if (prefix == null)
+							continue;
+						if (JDFConstants.XMLNS.equals(prefix))
+						{
+							if (!prefixes.contains(KElement.xmlnsLocalName(attName)))
+								continue;
+						}
 						// not in list - move on
-						if (prefix == null || !prefixes.contains(prefix))
+						else if (!prefixes.contains(prefix))
 							continue;
 					}
 					e1.removeAttribute(attName);
@@ -186,9 +162,9 @@ public class RemovePrivate extends BaseElementWalker
 
 	/**
 	 * zapp me
-	 * 
+	 *
 	 * @author prosirai
-	 * 
+	 *
 	 */
 	public class WalkPrivate extends WalkElement
 	{
@@ -225,7 +201,7 @@ public class RemovePrivate extends BaseElementWalker
 			}
 			if (prefixes != null)
 			{
-				String prefix = toCheck.getPrefix();
+				final String prefix = toCheck.getPrefix();
 				// not in list - we don't match
 				if (prefix == null || !prefixes.contains(prefix))
 					b = false;
@@ -241,9 +217,9 @@ public class RemovePrivate extends BaseElementWalker
 
 	/**
 	 * zapp me
-	 * 
+	 *
 	 * @author prosirai
-	 * 
+	 *
 	 */
 	public class WalkGeneralID extends WalkElement
 	{
@@ -257,8 +233,8 @@ public class RemovePrivate extends BaseElementWalker
 		@Override
 		public KElement walk(final KElement e, final KElement trackElem)
 		{
-			String idUsage = ((JDFGeneralID) e).getIDUsage();
-			String prefix = KElement.xmlnsPrefix(idUsage);
+			final String idUsage = ((JDFGeneralID) e).getIDUsage();
+			final String prefix = KElement.xmlnsPrefix(idUsage);
 			if (prefix != null && !"JDF".equalsIgnoreCase(prefix))
 			{
 				if (prefixes == null || prefixes.contains(prefix))
@@ -283,31 +259,34 @@ public class RemovePrivate extends BaseElementWalker
 	}
 
 	/**
-	 * 
+	 *
 	 * if set to true, generalID elements with proprietary namespace prefixes are zapped, else kept
+	 *
 	 * @param zappGeneralID if true, zapp 'em (the default) else keep 'em
 	 */
-	public void setZappGeneralID(boolean zappGeneralID)
+	public void setZappGeneralID(final boolean zappGeneralID)
 	{
 		this.zappGeneralID = zappGeneralID;
 	}
 
 	/**
-	 * 
+	 *
 	 * if set to true, attributes with proprietary namespace prefixes are zapped, else kept
+	 *
 	 * @param zappAttributes if true, zapp 'em (the default) else keep 'em
 	 */
-	public void setZappAttributes(boolean zappAttributes)
+	public void setZappAttributes(final boolean zappAttributes)
 	{
 		this.zappAttributes = zappAttributes;
 	}
 
 	/**
-	 * 
+	 *
 	 * if set to true, attributes with proprietary namespace prefixes are zapped, else kept
+	 *
 	 * @param zappElements if true, zapp 'em (the default) else keep 'em
 	 */
-	public void setZappElements(boolean zappElements)
+	public void setZappElements(final boolean zappElements)
 	{
 		this.zappElements = zappElements;
 	}
--- src/test/java/org/cip4/jdflib/elementwalker/RemovePrivateTest.java
+++ src/test/java/org/cip4/jdflib/elementwalker/RemovePrivateTest.java
@@ -2,68 +2,36 @@
  * The CIP4 Software License, Version 1.0
  *
  *
- * Copyright (c) 2001-2013 The International Cooperation for the Integration of
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights
- * reserved.
+ * Copyright (c) 2001-2019 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
  *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:
- *       "This product includes software developed by the
- *        The International Cooperation for the Integration of
- *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
- *    Alternately, this acknowledgment mrSubRefay appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
+ * 3. The end-user documentation included with the redistribution, if any, must include the following acknowledgment: "This product includes software developed by the The International Cooperation for
+ * the Integration of Processes in Prepress, Press and Postpress (www.cip4.org)" Alternately, this acknowledgment mrSubRefay appear in the software itself, if and wherever such third-party
+ * acknowledgments normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of
- *    Processes in  Prepress, Press and Postpress" must
- *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written
- *    permission, please contact info@cip4.org.
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of Processes in Prepress, Press and Postpress" must not be used to endorse or promote products derived from this software
+ * without prior written permission. For written permission, please contact info@cip4.org.
  *
- * 5. Products derived from this software may not be called "CIP4",
- *    nor may "CIP4" appear in their name, without prior writtenrestartProcesses()
- *    permission of the CIP4 organization
+ * 5. Products derived from this software may not be called "CIP4", nor may "CIP4" appear in their name, without prior writtenrestartProcesses() permission of the CIP4 organization
  *
- * Usage of this software in commercial products is subject to restrictions. For
- * details please consult info@cip4.org.
+ * Usage of this software in commercial products is subject to restrictions. For details please consult info@cip4.org.
  *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR
- * THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIrSubRefAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE INTERNATIONAL COOPERATION FOR THE INTEGRATION OF PROCESSES IN PREPRESS, PRESS AND POSTPRESS OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIrSubRefAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. ====================================================================
  *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration
- * of Processes in Prepress, Press and Postpress and was
- * originally based on software restartProcesses()
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
- * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ * This software consists of voluntary contributions made by many individuals on behalf of the The International Cooperation for the Integration of Processes in Prepress, Press and Postpress and was
+ * originally based on software restartProcesses() copyright (c) 1999-2001, Heidelberger Druckmaschinen AG copyright (c) 1999-2001, Agfa-Gevaert N.V.
  *
- * For more information on The International Cooperation for the
- * Integration of Processes in  Prepress, Press and Postpress , please see
- * <http://www.cip4.org/>.
+ * For more information on The International Cooperation for the Integration of Processes in Prepress, Press and Postpress , please see <http://www.cip4.org/>.
  *
  */
 package org.cip4.jdflib.elementwalker;
@@ -71,122 +39,139 @@ package org.cip4.jdflib.elementwalker;
 import org.cip4.jdflib.core.JDFDoc;
 import org.cip4.jdflib.core.KElement;
 import org.cip4.jdflib.node.JDFNode;
-import org.junit.Assert;
 import org.junit.Test;
 
 import junit.framework.TestCase;
+
 /**
-  * @author Rainer Prosi, Heidelberger Druckmaschinen *
+ * @author Rainer Prosi, Heidelberger Druckmaschinen *
  */
 public class RemovePrivateTest extends TestCase
 {
 	/**
-	 * 
+	 *
 	 */
 	@Test
 	public void testRemove()
 	{
-		JDFDoc d = new JDFDoc("JDF");
-		JDFNode n = d.getJDFRoot();
+		final JDFDoc d = new JDFDoc("JDF");
+		final JDFNode n = d.getJDFRoot();
 		n.setAttribute("foo:bar", "www.foo.com", "blub");
 		n.appendElement("blah:e", "www.blah.com");
 		n.getCreateAuditPool().setAttribute("foo:bar", "www.foo.com", "blub");
 		n.getCreateAuditPool().appendElement("blah:e", "www.blah.com");
-		RemovePrivate rp = new RemovePrivate();
+		final RemovePrivate rp = new RemovePrivate();
 		rp.walkTree(n, null);
-		Assert.assertNull(n.getAttribute("foo:bar", "www.foo.com", null));
-		Assert.assertNull(n.getElement("e", "www.blah.com", 0));
-		Assert.assertNotNull(n.getAuditPool());
+		assertNull(n.getAttribute("foo:bar", "www.foo.com", null));
+		assertNull(n.getElement("e", "www.blah.com", 0));
+		assertNotNull(n.getAuditPool());
 	}
 
 	/**
-	 * 
+	 *
+	 */
+	@Test
+	public void testRemovePrefix()
+	{
+		final KElement e = KElement.createRoot("a:b", "www.a.com");
+		e.setAttribute("xmlns:foo", "www.foo.com");
+		e.setAttribute("foo:bar", "blub");
+		final RemovePrivate rp = new RemovePrivate();
+		rp.addPrefix("foo");
+		rp.walkTree(e, null);
+		assertNull(e.getNonEmpty("foo:bar"));
+		assertNull(e.getNonEmpty("bar"));
+		assertNull(e.getNonEmpty("xmlns:foo"));
+	}
+
+	/**
+	 *
 	 */
 	@Test
 	public void testZappAttributes()
 	{
-		JDFDoc d = new JDFDoc("JDF");
-		JDFNode n = d.getJDFRoot();
+		final JDFDoc d = new JDFDoc("JDF");
+		final JDFNode n = d.getJDFRoot();
 		n.setAttribute("foo:bar", "blub", "www.foo.com");
 		n.appendElement("blah:e", "www.blah.com");
 		n.getCreateAuditPool().setAttribute("foo:bar", "blub", "www.foo.com");
 		n.getCreateAuditPool().appendElement("blah:e", "www.blah.com");
-		RemovePrivate rp = new RemovePrivate();
+		final RemovePrivate rp = new RemovePrivate();
 		rp.setZappAttributes(false);
 		rp.walkTree(n, null);
-		Assert.assertEquals("blub", n.getAttribute("foo:bar", "www.foo.com", null));
-		Assert.assertNull(n.getElement("e", "www.blah.com", 0));
-		Assert.assertNotNull(n.getAuditPool());
+		assertEquals("blub", n.getAttribute("foo:bar", "www.foo.com", null));
+		assertNull(n.getElement("e", "www.blah.com", 0));
+		assertNotNull(n.getAuditPool());
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	@Test
 	public void testZappElements()
 	{
-		JDFDoc d = new JDFDoc("JDF");
-		JDFNode n = d.getJDFRoot();
+		final JDFDoc d = new JDFDoc("JDF");
+		final JDFNode n = d.getJDFRoot();
 		n.setAttribute("foo:bar", "blub", "www.foo.com");
-		KElement e1 = n.appendElement("blah:e", "www.blah.com");
+		final KElement e1 = n.appendElement("blah:e", "www.blah.com");
 		n.getCreateAuditPool().setAttribute("foo:bar", "blub", "www.foo.com");
 		n.getCreateAuditPool().appendElement("blah:e", "www.blah.com");
-		RemovePrivate rp = new RemovePrivate();
+		final RemovePrivate rp = new RemovePrivate();
 		rp.setZappElements(false);
 		rp.walkTree(n, null);
-		Assert.assertNull(n.getAttribute("foo:bar", "www.foo.com", null));
-		Assert.assertEquals(e1, n.getElement("e", "www.blah.com", 0));
-		Assert.assertNotNull(n.getAuditPool());
+		assertNull(n.getAttribute("foo:bar", "www.foo.com", null));
+		assertEquals(e1, n.getElement("e", "www.blah.com", 0));
+		assertNotNull(n.getAuditPool());
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	@Test
 	public void testGeneralID()
 	{
-		JDFDoc d = new JDFDoc("JDF");
-		JDFNode n = d.getJDFRoot();
+		final JDFDoc d = new JDFDoc("JDF");
+		final JDFNode n = d.getJDFRoot();
 		n.appendGeneralID("foo:key", "bar");
-		RemovePrivate rp = new RemovePrivate();
+		final RemovePrivate rp = new RemovePrivate();
 		rp.walkTree(n, null);
-		Assert.assertNull(n.getGeneralID(0));
+		assertNull(n.getGeneralID(0));
 		n.appendGeneralID("key", "bar");
 		rp.walkTree(n, null);
-		Assert.assertEquals(n.getGeneralID("key", 0), "bar");
+		assertEquals(n.getGeneralID("key", 0), "bar");
 		n.appendGeneralID("foo:key", "bar");
 		rp.addPrefix("blub");
 		rp.walkTree(n, null);
-		Assert.assertEquals(n.getGeneralID("foo:key", 0), "bar");
+		assertEquals(n.getGeneralID("foo:key", 0), "bar");
 		rp.addPrefix("foo");
 		rp.setZappGeneralID(false);
 		rp.walkTree(n, null);
-		Assert.assertEquals(n.getGeneralID("foo:key", 0), "bar");
+		assertEquals(n.getGeneralID("foo:key", 0), "bar");
 		rp.setZappGeneralID(true);
 		rp.walkTree(n, null);
-		Assert.assertNull(n.getGeneralID("foo:key", 0));
+		assertNull(n.getGeneralID("foo:key", 0));
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	@Test
 	public void testAddPrefix()
 	{
-		JDFDoc d = new JDFDoc("JDF");
-		JDFNode n = d.getJDFRoot();
+		final JDFDoc d = new JDFDoc("JDF");
+		final JDFNode n = d.getJDFRoot();
 		n.setAttribute("foo:bar", "blub", "www.foo.com");
 		n.setAttribute("blah:blah", "blub", "www.blah.com");
 		n.appendElement("blah:e", "www.blah.com");
 		n.getCreateAuditPool().setAttribute("foo:bar", "blub", "www.foo.com");
 		n.getCreateAuditPool().appendElement("blah:e", "www.blah.com");
-		RemovePrivate rp = new RemovePrivate();
+		final RemovePrivate rp = new RemovePrivate();
 		rp.addPrefix("foo");
 		rp.walkTree(n, null);
-		Assert.assertNull(n.getAttribute("bar", "www.foo.com", null));
-		Assert.assertNotNull(n.getAttribute("blah", "www.blah.com", null));
-		Assert.assertNotNull(n.getElement("e", "www.blah.com", 0));
-		Assert.assertNotNull(n.getAuditPool());
+		assertNull(n.getAttribute("bar", "www.foo.com", null));
+		assertNotNull(n.getAttribute("blah", "www.blah.com", null));
+		assertNotNull(n.getElement("e", "www.blah.com", 0));
+		assertNotNull(n.getAuditPool());
 
 	}
 }
