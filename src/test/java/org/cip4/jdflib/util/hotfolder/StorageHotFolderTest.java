ad74057d33f6446d6785a30c611f8b68be7c6b95
Rainer Prosi <rainer.prosi@heidelberg.com>
2024-06-28 12:24:24 +0200

    restart build

--- src/test/java/org/cip4/jdflib/util/hotfolder/StorageHotFolderTest.java
+++ src/test/java/org/cip4/jdflib/util/hotfolder/StorageHotFolderTest.java
@@ -1,7 +1,7 @@
 /**
  * The CIP4 Software License, Version 1.0
  *
- * Copyright (c) 2001-2023 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
+ * Copyright (c) 2001-2024 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
