23a0431c8bcf1e0fb839588b332aca40ba97795e
Rainer Prosi <rainer.prosi@heidelberg.com>
2024-10-17 13:01:58 +0200

    improved xjdf comment conversion

--- src/test/java/org/cip4/jdflib/extensions/xjdfwalker/xjdftojdf/WalkCommentTest.java
+++ src/test/java/org/cip4/jdflib/extensions/xjdfwalker/xjdftojdf/WalkCommentTest.java
@@ -1,8 +1,8 @@
 /**
  * The CIP4 Software License, Version 1.0
  *
- * Copyright (c) 2001-2024 The International Cooperation for the Integration of 
- * Processes in  Prepress, Press and Postpress (CIP4).  All rights 
+ * Copyright (c) 2001-2024 The International Cooperation for the Integration of
+ * Processes in  Prepress, Press and Postpress (CIP4).  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -10,7 +10,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -18,17 +18,17 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
- *        The International Cooperation for the Integration of 
+ *        The International Cooperation for the Integration of
  *        Processes in  Prepress, Press and Postpress (www.cip4.org)"
  *    Alternately, this acknowledgment may appear in the software itself,
  *    if and wherever such third-party acknowledgments normally appear.
  *
- * 4. The names "CIP4" and "The International Cooperation for the Integration of 
+ * 4. The names "CIP4" and "The International Cooperation for the Integration of
  *    Processes in  Prepress, Press and Postpress" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact info@cip4.org.
  *
  * 5. Products derived from this software may not be called "CIP4",
@@ -54,78 +54,45 @@
  * ====================================================================
  *
  * This software consists of voluntary contributions made by many
- * individuals on behalf of the The International Cooperation for the Integration 
+ * individuals on behalf of the The International Cooperation for the Integration
  * of Processes in Prepress, Press and Postpress and was
- * originally based on software 
- * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG 
- * copyright (c) 1999-2001, Agfa-Gevaert N.V. 
- *  
- * For more information on The International Cooperation for the 
+ * originally based on software
+ * copyright (c) 1999-2001, Heidelberger Druckmaschinen AG
+ * copyright (c) 1999-2001, Agfa-Gevaert N.V.
+ *
+ * For more information on The International Cooperation for the
  * Integration of Processes in  Prepress, Press and Postpress , please see
  * <http://www.cip4.org/>.
- *  
- * 
+ *
+ *
  */
 package org.cip4.jdflib.extensions.xjdfwalker.xjdftojdf;
 
-import org.cip4.jdflib.core.AttributeName;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.cip4.jdflib.JDFTestCaseBase;
 import org.cip4.jdflib.core.JDFComment;
-import org.cip4.jdflib.core.KElement;
+import org.cip4.jdflib.core.JDFDoc;
 import org.cip4.jdflib.extensions.XJDFConstants;
+import org.cip4.jdflib.extensions.XJDFHelper;
+import org.cip4.jdflib.extensions.xjdfwalker.XJDFToJDFConverter;
+import org.junit.jupiter.api.Test;
 
-/**
- * @author Rainer Prosi, Heidelberger Druckmaschinen
- * 
- *         walker for the JMF or XJMF root
- */
-public class WalkComment extends WalkXElement
+class WalkCommentTest extends JDFTestCaseBase
 {
-	/**
-	 * 
-	 */
-	public WalkComment()
-	{
-		super();
-	}
 
 	/**
-	 * @param e
-	 * @return true if must continue
+	 *
 	 */
-	@Override
-	public KElement walk(final KElement e, final KElement trackElem)
+	@Test
+	void testID()
 	{
-		// fixAuthor(e);
-		final KElement newJDF = super.walk(e, trackElem);
-		newJDF.setText(e.getText());
-		return newJDF;
+		final XJDFHelper h = new XJDFHelper("j1", "p1");
+
+		final JDFComment c = h.setComment("t");
+		c.setAttribute(XJDFConstants.ExternalID, "x");
+		final JDFDoc d = new XJDFToJDFConverter(null).convert(h);
+		assertNotEquals("x", d.getJDFRoot().getComment(0).getID());
 	}
 
-	/**
-	 * @see org.cip4.jdflib.elementwalker.BaseWalker#matches(org.cip4.jdflib.core.KElement)
-	 * @param toCheck
-	 * @return true if it matches
-	 */
-	@Override
-	public boolean matches(final KElement toCheck)
-	{
-		return toCheck instanceof JDFComment;
-	}
-
-	/**
-	 * @see org.cip4.jdflib.extensions.xjdfwalker.xjdftojdf.WalkXElement#updateAttributes(org.cip4.jdflib.core.KElement)
-	 */
-	@Override
-	protected void updateAttributes(final KElement elem)
-	{
-		elem.renameAttribute(AttributeName.TYPE, AttributeName.NAME);
-		final String xid = elem.getNonEmpty(XJDFConstants.ExternalID);
-		if (xid != null)
-		{
-			elem.removeAttribute(XJDFConstants.ExternalID);
-			elem.setID(xid + KElement.uniqueID(0, true));
-		}
-
-		super.updateAttributes(elem);
-	}
 }
