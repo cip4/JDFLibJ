453d8bf8d980ed7ec7805c53cbbfabd8bf830da0
Rainer Prosi <rainer.prosi@heidelberg.com>
2025-04-16 14:10:01 +0200

    cosmetics

--- src/main/java/org/cip4/jdflib/util/mime/MimeReader.java
+++ src/main/java/org/cip4/jdflib/util/mime/MimeReader.java
@@ -3,7 +3,7 @@
  * The CIP4 Software License, Version 1.0
  *
  *
- * Copyright (c) 2001-2023 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
+ * Copyright (c) 2001-2025 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
@@ -147,7 +147,6 @@ public class MimeReader extends MimeHelper
 
 		try
 		{
-			// final InputStream newStream = new PrefixInputStream("\n", StreamUtil.getBufferedInputStream(mimeStream));
 			final InputStream newStream = StreamUtil.getBufferedInputStream(mimeStream);
 			final Message mimeMessage = new MimeMessage(null, newStream);
 
