cd8bc557243e105154d60bda2e67e27a2ae8694b
Rainer Prosi <rainer.prosi@heidelberg.com>
2025-08-01 10:48:08 +0200

    security hole fix for XMLParser   this.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);

--- src/main/java/org/cip4/jdflib/core/XMLParser.java
+++ src/main/java/org/cip4/jdflib/core/XMLParser.java
@@ -3,7 +3,7 @@
  * The CIP4 Software License, Version 1.0
  *
  *
- * Copyright (c) 2001-2024 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
+ * Copyright (c) 2001-2025 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
@@ -372,6 +372,7 @@ public class XMLParser extends DOMParser
 			this.setProperty("http://apache.org/xml/properties/dom/document-class-name", getDocumentClass());
 			this.setFeature("http://apache.org/xml/features/dom/defer-node-expansion", false);
 			this.setFeature("http://xml.org/sax/features/namespaces", true);
+			this.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
 
 		}
 		catch (final SAXNotRecognizedException e)
