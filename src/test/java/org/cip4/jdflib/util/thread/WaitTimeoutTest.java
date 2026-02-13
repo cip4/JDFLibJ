230d6fd67f6c33967c81689bcd4032717af056ce
Rainer Prosi <rainer.prosi@heidelberg.com>
2026-02-13 10:28:11 +0100

    restart flaky build

--- src/test/java/org/cip4/jdflib/util/thread/WaitTimeoutTest.java
+++ src/test/java/org/cip4/jdflib/util/thread/WaitTimeoutTest.java
@@ -1,7 +1,7 @@
 /**
  * The CIP4 Software License, Version 1.0
  *
- * Copyright (c) 2001-2015 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
+ * Copyright (c) 2001-2026 The International Cooperation for the Integration of Processes in Prepress, Press and Postpress (CIP4). All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
@@ -69,13 +69,13 @@ class WaitTimeoutTest extends JDFTestCaseBase
 	@Test
 	void testGetFail()
 	{
-		Assertions.assertNull(new TestWait(12345, 12).getWaitedObject());
+		Assertions.assertNull(new TestWait(42345, 12).getWaitedObject());
 	}
 
 	@Test
 	void testGetOK()
 	{
-		Assertions.assertTrue(new TestWait(12, 12345).getWaitedObject().booleanValue());
+		Assertions.assertTrue(new TestWait(12, 42345).getWaitedObject());
 	}
 
 	@Test
